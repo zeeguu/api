6f6aea3beb4ae51d150723e54aeabab34efbdffb
Mircea Lungu <mircea.lungu@gmail.com>
2017-05-14 14:38:35 +0200

    exception handling

--- zeeguu_api/api/translate_and_bookmark.py
+++ zeeguu_api/api/translate_and_bookmark.py
@@ -257,8 +257,11 @@ def main_translation(word_str, context_str, from_lang_code, to_lang_code):
     # Assume we're translating the first occurrence of the given word in the context
     left_context, right_context = context_str.split(word_str, 1)
 
-    zeeguu.log ("\n\n ===== Translation request from: {2} to: {3} \n -- context: {1} \n -- word: {0} \n -- left context: {4}\n -- right context: {5}".
+    try: 
+        zeeguu.log ("\n\n ===== Translation from: {2} to: {3} \n -- word: {0} \n -- left context: {4} \n -- right context: {5}".
            format(word_str, context_str, from_lang_code, to_lang_code, left_context, right_context))
+    except Exception as e:
+        print(str(e))
 
     try:
         t = GoogleTranslatorFactory.build(from_lang_code, to_lang_code)
