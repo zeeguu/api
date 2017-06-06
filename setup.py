1abbad195d8fbf3e9138cc1e44ef8a62d3359780
Mircea Lungu <mircea.lungu@gmail.com>
2017-06-06 19:50:23 +0200

    .

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ import setuptools
 
 setuptools.setup(
     name="zeeguu_api",
-    version="0.1",
+    version="0.2",
     packages=setuptools.find_packages(),
     include_package_data=True,
     zip_safe=False,
