58e1a3da7831572d43a7be2376cc767bad513550
Mircea Lungu <mircea.lungu@gmail.com>
2017-06-06 20:16:45 +0200

    running even w/o the dashboard

--- setup.py
+++ setup.py
@@ -3,17 +3,16 @@
 
 import setuptools
 
-
 setuptools.setup(
     name="zeeguu_api",
     version="0.2",
     packages=setuptools.find_packages(),
     include_package_data=True,
     zip_safe=False,
-    author="Zeeguu Team",
-    author_email="me@mir.lu",
-    description="API for Zeeguu",
-    keywords="Second language acquisition api",
+    author="The Zeeguu Team",
+    author_email="zeeguu.team@gmail.com",
+    description="API for Zeeguu, a project that aims to accelerate vocabulary acquisition in a second language",
+    keywords=" API, second language acquisition",
     dependency_links=[
             "git+https://github.com/mircealungu/zeeguu-core.git#egg=zeeguu",
             "git+https://github.com/mircealungu/python-translators.git#egg=python_translators",
