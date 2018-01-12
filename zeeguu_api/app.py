2f934b58fed2da14824881dbb6b2b525856906de
Mircea Lungu <mircea.lungu@gmail.com>
2018-01-12 20:52:21 +0100

    disabling the dashboard made easy

--- zeeguu_api/app.py
+++ zeeguu_api/app.py
@@ -11,6 +11,7 @@ CORS(app)
 load_configuration_or_abort(app, 'ZEEGUU_API_CONFIG',
                             ['SQLALCHEMY_DATABASE_URI', 'HOST', 'DEBUG', 'SECRET_KEY', 'MAX_SESSION'])
 
+dashboard_enabled = False
 
 # The zeeguu.model  module relies on an app being injected from outside
 # ----------------------------------------------------------------------
@@ -23,32 +24,33 @@ assert zeeguu.model
 from .api import api
 app.register_blueprint(api)
 
-try:
-    import dashboard
-    dashboard.config.from_file('/home/mircea/zee/http/api/dashboard.cfg')
+if dashboard_enabled:
+    try:
+        import dashboard
+        dashboard.config.from_file('/home/mircea/zee/http/api/dashboard.cfg')
 
-    # dashboard can benefit from a way of associating a request with a user id
-    def get_user_id():
-        import flask
-        print ("trying to get the flask.g.user.id")
-        try:
-            session_id = int(flask.request.args['session'])
-        except:
-            print ("cound not find the session in the request")
-            return 1
-        from zeeguu.model import Session
-        session = Session.find_for_id(session_id)
-        print ("found session object")
+        # dashboard can benefit from a way of associating a request with a user id
+        def get_user_id():
+            import flask
+            print ("trying to get the flask.g.user.id")
+            try:
+                session_id = int(flask.request.args['session'])
+            except:
+                print ("cound not find the session in the request")
+                return 1
+            from zeeguu.model import Session
+            session = Session.find_for_id(session_id)
+            print ("found session object")
 
-        user_id = session.user.id
-        print ("got user id = " + str(user_id))
-        return user_id
+            user_id = session.user.id
+            print ("got user id = " + str(user_id))
+            return user_id
 
 
-    dashboard.config.get_group_by = get_user_id
-    dashboard.bind(app=app)
-except Exception as e:
-    print ("could not start the dashboard, but will continue anyway")
-    print (str(e))
-    raise e
+        dashboard.config.get_group_by = get_user_id
+        dashboard.bind(app=app)
+    except Exception as e:
+        print ("could not start the dashboard, but will continue anyway")
+        print (str(e))
+        raise e
 
