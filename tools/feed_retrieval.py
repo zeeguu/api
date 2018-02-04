d8f25285871c1e810f140f5342c414df258c7d49
Mircea Lungu <mircea.lungu@gmail.com>
2018-02-04 14:35:50 +0100

    rename and a bit of refactoring

--- tools/feed_retrieval.py
+++ tools/feed_retrieval.py
@@ -1,4 +1,21 @@
 #!/usr/bin/env python
+
+"""
+
+   Script that goes through all the feeds that are
+   available in the DB and retrieves the newest articles
+   in order to populate the DB with them.
+
+   The DB is populated by saving Article objects in the
+   articles table.
+
+   Before this script checking whether there were new items
+   in a given feed was done while serving the request for
+   items to read. That was too slow.
+
+   To be called from a cron job.
+
+"""
 import datetime
 import watchmen
 
@@ -6,37 +23,30 @@ import zeeguu
 from zeeguu import model
 from zeeguu.model import Url, RSSFeed
 
-print(" ")
+LOG_CONTEXT = "FEED RETRIEVAL"
+
 session = zeeguu.db.session
 
-feeds = RSSFeed.query.all()
+for feed in RSSFeed.query.all():
 
-for feed in feeds:
-    print(f"{feed}\n")
-
-    feed_items = feed.feed_items()
-
-    for feed_item in feed_items:
+    for feed_item in feed.feed_items():
 
         title = feed_item['title']
-
         url = feed_item['url']
-        print(f" -{url}")
 
         art = model.Article.find(url)
 
         if art:
-            print(f"Already found: {art}")
-
+            print(f"Already found in the DB: {art}")
         else:
             try:
                 art = watchmen.article_parser.get_article(url)
-                # print(art.text)
 
                 word_count = len(art.text.split(" "))
 
                 if word_count < 10:
-                    print("?? seems like we don't actually have text here?")
+                    zeeguu.lognprint(f" {LOG_CONTEXT}: Can't find text for: {url}")
+
                 else:
                     from zeeguu.language.difficulty_estimator_factory import DifficultyEstimatorFactory
 
@@ -57,6 +67,8 @@ for feed in feeds:
                     )
                     session.add(new_article)
                     session.commit()
-                    print(f"Added: {new_article}")
-            except:
-                print (" -- something went wrong")
+                    zeeguu.lognprint()
+                    print(f" {LOG_CONTEXT}: Added: {new_article}")
+            except Exception as ex:
+                zeeguu.lognprint(f" {LOG_CONTEXT}: Failed to create zeeguu.Article from {url}")
+                zeeguu.log(str(ex))
