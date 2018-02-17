d55dfe96a894ab06d6dede0095f2872729f2ca2b
Mircea Lungu <mircea.lungu@gmail.com>
2018-02-17 10:42:42 +0100

    formatting

--- zeeguu_api/api/exercises.py
+++ zeeguu_api/api/exercises.py
@@ -27,11 +27,9 @@ def bookmarks_to_study(bookmark_count):
         # computed since theuser never did an exercise, and currently only
         # then are priorities recomputed; thus, in this case, we try to
         # update, and maybe this will solve the problem
-        zeeguu.log ("recomputting bookmark priorities since there seem to be no bookmarks to study")
+        zeeguu.log("recomputting bookmark priorities since there seem to be no bookmarks to study")
         zeeguu.word_scheduling.arts.update_bookmark_priority(zeeguu.db, flask.g.user)
         to_study = flask.g.user.bookmarks_to_study(int_count)
-
-
 
     as_json = [bookmark.json_serializable_dict() for bookmark in to_study]
     return json_result(as_json)
@@ -46,11 +44,11 @@ def get_exercise_log_for_bookmark(bookmark_id):
     exercise_log_dict = []
     exercise_log = bookmark.exercise_log
     for exercise in exercise_log:
-        exercise_log_dict.append(dict(id = exercise.id,
-                                      outcome = exercise.outcome.outcome,
-                                      source = exercise.source.source,
-                                      exercise_log_solving_speed = exercise.solving_speed,
-                                      time = exercise.time.strftime('%m/%d/%Y')))
+        exercise_log_dict.append(dict(id=exercise.id,
+                                      outcome=exercise.outcome.outcome,
+                                      source=exercise.source.source,
+                                      exercise_log_solving_speed=exercise.solving_speed,
+                                      time=exercise.time.strftime('%m/%d/%Y')))
 
     return json_result(exercise_log_dict)
 
@@ -58,7 +56,7 @@ def get_exercise_log_for_bookmark(bookmark_id):
 @api.route("/report_exercise_outcome/<exercise_outcome>/<exercise_source>/<exercise_solving_speed>/<bookmark_id>",
            methods=["POST"])
 @with_session
-def report_exercise_outcome(exercise_outcome,exercise_source,exercise_solving_speed,bookmark_id):
+def report_exercise_outcome(exercise_outcome, exercise_source, exercise_solving_speed, bookmark_id):
     """
     In the model parlance, an exercise is an entry in a table that
     logs the performance of an exercise. Every such performance, has a source, and an outcome.
@@ -84,19 +82,17 @@ def report_exercise_outcome(exercise_outcome,exercise_source,exercise_solving_sp
         if not new_outcome:
             return "could not find outcome"
 
-        exercise = Exercise(new_outcome,new_source,exercise_solving_speed, datetime.now())
+        exercise = Exercise(new_outcome, new_source, exercise_solving_speed, datetime.now())
         bookmark.add_new_exercise(exercise)
         bookmark.update_fit_for_study(zeeguu.db.session)
         bookmark.update_learned_status(zeeguu.db.session)
         zeeguu.db.session.add(exercise)
         zeeguu.db.session.commit()
 
-        zeeguu.log ("recomputting bookmark priorities")
+        zeeguu.log("recomputting bookmark priorities")
         zeeguu.word_scheduling.arts.update_bookmark_priority(zeeguu.db, flask.g.user)
 
         return "OK"
-    except :
+    except:
         traceback.print_exc()
         return "FAIL"
-
-
