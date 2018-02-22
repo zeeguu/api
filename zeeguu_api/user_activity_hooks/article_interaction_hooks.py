d5a51896f17fa466a963cb2ace20f1de54abd349
Mircea Lungu <mircea.lungu@gmail.com>
2018-02-22 21:08:08 +0100

    handling also like/unlike events

--- zeeguu_api/user_activity_hooks/article_interaction_hooks.py
+++ zeeguu_api/user_activity_hooks/article_interaction_hooks.py
@@ -20,21 +20,40 @@ def distill_article_interactions(session, user, data):
     value = data['value']
     extra_data = data['extra_data']
 
-    log (f'event is: {event}')
+    log(f'event is: {event}')
     if "UMR - OPEN ARTICLE" in event:
         article_opened(session, value, user)
-    # elif "UMR - LIKE ARTICLE" in event:
-    #     article_like()
+    elif "UMR - LIKE ARTICLE" in event:
+        article_liked(session, value, user, True)
+    elif "UMR - UNLIKE ARTICLE" in event:
+        article_liked(session, value, user, False)
 
 
-def article_opened(session, url, user):
-
+def article_liked(session, value, user, like_value):
     # the url that comes from zeeguu event logger
     # might be the zeeguu url: which is of the form
     # https://www.zeeguu.unibe.ch/read/article?articleLanguage=de&articleURL=https://www.nzz.ch/wissenschaft/neandertaler-waren-kuenstler-ld.1358862
     # thus we extract only the last part
-    url = url.split('articleURL=')[-1]
+    url = value.split('articleURL=')[-1]
 
     article = Article.find_or_create(session, url)
-    ua = UserArticle.find_or_create(session, user, article, opened=datetime.now())
+    ua = UserArticle.find_or_create(session, user, article)
+    ua.liked = like_value
+    session.add(ua)
+    session.commit()
+    log(f"created new {ua}")
+
+
+def article_opened(session, value, user):
+    # the url that comes from zeeguu event logger
+    # might be the zeeguu url: which is of the form
+    # https://www.zeeguu.unibe.ch/read/article?articleLanguage=de&articleURL=https://www.nzz.ch/wissenschaft/neandertaler-waren-kuenstler-ld.1358862
+    # thus we extract only the last part
+    url = value.split('articleURL=')[-1]
+
+    article = Article.find_or_create(session, url)
+    ua = UserArticle.find_or_create(session, user, article)
+    ua.opened = datetime.now()
+    session.add(ua)
+    session.commit()
     log(f"created new {ua}")
