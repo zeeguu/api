a4e3580fce2172fa4500d583b533370a04c2b9e6
Mircea Lungu <mircea.lungu@gmail.com>
2018-02-22 20:20:19 +0100

    fix

--- zeeguu_api/user_activity_hooks/article_interaction_hooks.py
+++ zeeguu_api/user_activity_hooks/article_interaction_hooks.py
@@ -20,6 +20,7 @@ def distill_article_interactions(session, user, data):
     value = data['value']
     extra_data = data['extra_data']
 
+    log (f'event is: {event}')
     if "UMR - OPEN ARTICLE" in event:
         article_opened(session, value, user)
     # elif "UMR - LIKE ARTICLE" in event:
@@ -27,6 +28,13 @@ def distill_article_interactions(session, user, data):
 
 
 def article_opened(session, url, user):
+
+    # the url that comes from zeeguu event logger
+    # might be the zeeguu url: which is of the form
+    # https://www.zeeguu.unibe.ch/read/article?articleLanguage=de&articleURL=https://www.nzz.ch/wissenschaft/neandertaler-waren-kuenstler-ld.1358862
+    # thus we extract only the last part
+    url = url.split('articleURL=')[-1]
+    
     article = Article.find_or_create(session, url)
     ua = UserArticle.find_or_create(session, user, article, opened=datetime.now())
     log(f"created new {ua}")
