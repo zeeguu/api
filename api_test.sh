14f98b5204619e7c1bcf7da9c789b8d54f240036
Mircea Lungu <mircea.lungu@gmail.com>
2018-03-13 16:03:52 +0100

    nicer script for api_test that creates a zeeguu_test DB if needed

--- api_test.sh
+++ api_test.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
-echo "Provide your mysql root password (or modify the script accordingly)"
-mysql -e "create database IF NOT EXISTS zeeguu_test; grant all on zeeguu_test.* to 'zeeguu_test'@'localhost' identified by 'zeeguu_test';" -uroot -p
+if MYSQL_PWD=zeeguu_test mysql -e "use zeeguu_test;" -uzeeguu_test; then
+	echo "Found zeeguu_test databse"
+else
+	echo "The zeeguu_test databse does not exist. Trying to create it..."
+
+	echo "Provide your mysql root password (or modify the script accordingly)"
+	mysql -e "create database IF NOT EXISTS zeeguu_test; grant all on zeeguu_test.* to 'zeeguu_test'@'localhost' identified by 'zeeguu_test';" -uroot -p
+fi
+
+
 export ZEEGUU_API_CONFIG="./default_api.cfg"
 python -m zeeguu_api
