171ac86eb498cb7e02447d7a8ba9fb22de8ef7b1
Mircea Lungu <mircea.lungu@gmail.com>
2018-03-13 16:15:09 +0100

    smarter api_test script

--- api_test.sh
+++ api_test.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-if MYSQL_PWD=zeeguu_test mysql -e "use zeeguu_test;" -uzeeguu_test; then
+if mysql -e "use zeeguu_test;" -uzeeguu_test -pzeeguu_test 1>/dev/null 2>/dev/null; then
 	echo "Found zeeguu_test databse"
 else
-	echo "The zeeguu_test databse does not exist. Trying to create it..."
+	echo "The zeeguu_test database does not exist. "
 
-	echo "Provide your mysql root password (or modify the script accordingly)"
+	echo "To create you must either "
+	echo " - provide the password for the mysql root user, or"
+	echo " - modify the script accordingly"
 	mysql -e "create database IF NOT EXISTS zeeguu_test; grant all on zeeguu_test.* to 'zeeguu_test'@'localhost' identified by 'zeeguu_test';" -uroot -p
 fi
 
