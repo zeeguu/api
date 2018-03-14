a8dd603478b25dad1f622abb1c2bedd400758b1a
Mircea Filip Lungu <mircea.lungu@gmail.com>
2018-03-14 19:18:37 +0100

    Update run_tests.sh

--- run_tests.sh
+++ run_tests.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 export PYTHONWARNINGS='ignore'
-python -m pytest || echo "make sure to install pytest (pip install pytest)"
+python3.6 -m pytest --version 1>/dev/null 2>/dev/null || (echo "installing pytest..." && pip3.6 install pytest) && python3.6 -m pytest
 export PYTHONWARNINGS='default'
