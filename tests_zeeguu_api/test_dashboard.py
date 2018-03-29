c62ab6d07422601b9ff725843606adf927da77eb
Oli26 <lolol1990@hotmail.co.uk>
2018-03-29 08:23:04 -0400

    test for has)session working

--- tests_zeeguu_api/test_dashboard.py
+++ tests_zeeguu_api/test_dashboard.py
@@ -25,18 +25,22 @@ class DashboardTest(APITestMixin, TestCase):
 
     def test_has_session(self):
         from zeeguu_api.app import app
-        data = {}
-        result = self.app.get('has_session?session=31235ds', data)
+        self.app = app.test_client()
 
+        # TEST FOR FAKE SESSION
+        result = self.app.get('/has_session?session=31235ds')
+        result = json.loads(result.data)
         assert result==0
+
+        # TEST FOR REAL SESSION
         userDictionary = {
             'username':'testUser',
             'password':'password'
         }
         rv = self.api_post('/add_user/test321@gmail.com', userDictionary)
-        data = {}
-        params = {'session':rv}
-        result = self.app.get('/has_session?session='+params['session'], data, params)
+        assert rv
+        result = self.app.get('/has_session?session='+rv.data.decode('utf-8'))
+        result = json.loads(result.data)
         assert result==1
 
 
