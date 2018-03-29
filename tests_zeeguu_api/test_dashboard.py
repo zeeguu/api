5fafcd5366699f237c4acf13b5596612369a8143
Oli26 <lolol1990@hotmail.co.uk>
2018-03-29 07:46:27 -0400

    unit testing for dashboard 0.1

--- tests_zeeguu_api/test_dashboard.py
+++ tests_zeeguu_api/test_dashboard.py
@@ -6,6 +6,38 @@ from tests_zeeguu_api.api_test_mixin import APITestMixin
 
 import zeeguu
 
+
 class DashboardTest(APITestMixin, TestCase):
+
+
+    #def test_adding_classes(self):
+
+
+
+    #def test_get_classes(self):
+
+
+   # def test_get_users_from_class(self):
+
+
+    #def test_get_user_data(self):
+
+
+    def test_has_session(self):
+        from zeeguu_api.app import app
+        data = {}
+        result = self.app.get('has_session?session=31235ds', data)
+
+        assert result==0
+        userDictionary = {
+            'username':'testUser',
+            'password':'password'
+        }
+        rv = self.api_post('/add_user/test321@gmail.com', userDictionary)
+        data = {}
+        params = {'session':rv}
+        result = self.app.get('/has_session?session='+params['session'], data, params)
+        assert result==1
+
 
 
