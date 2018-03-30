b369c14a3544c6cd05ca00f0ae6e6ad341c18cdb
Oli26 <lolol1990@hotmail.co.uk>
2018-03-30 17:12:02 -0400

    misseda definition

--- zeeguu_api/api/dashboard.py
+++ zeeguu_api/api/dashboard.py
@@ -139,7 +139,7 @@ def get_classes_by_teacher_id():
 # Takes cohort_id and reuturns dictionary with relevant class variables
 @api.route("/get_class_info/<id>")
 @with_session
-wrapper_to_json(id):
+def wrapper_to_json(id):
     if(not class_function_checker(id)):
         flask.abort(401)
     return jsonify(get_class_info(id))
