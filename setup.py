bc93e1d7206e92a8cd2b0c651b777de0e26c02cf
Mircea Lungu <mircea.lungu@gmail.com>
2018-03-31 00:41:51 +0300

    added requirements file.

--- setup.py
+++ setup.py
@@ -13,8 +13,10 @@ setuptools.setup(
     author_email="zeeguu.team@gmail.com",
     description="API for Zeeguu, a project that aims to accelerate vocabulary acquisition in a second language",
     keywords=" API, second language acquisition",
+    
     dependency_links=[
         "https://github.com/zeeguu-ecosystem/Zeeguu-Core/tarball/master#egg=zeeguu",
+        "git+https://github.com/mircealungu/python-wordstats.git#egg=wordstats",
         "https://github.com/zeeguu-ecosystem/Python-Translators/tarball/master#egg=python_translators"],
     install_requires=("flask>=0.10.1",
                       "Flask-SQLAlchemy",
@@ -25,6 +27,7 @@ setuptools.setup(
                       "beautifulsoup4",
                       "feedparser",
                       'zeeguu',
+                      'wordstats',
                       'python_translators',
                       'flask_monitoringdashboard>=1.10.4'
                       )
