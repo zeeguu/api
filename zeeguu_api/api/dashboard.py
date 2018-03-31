e35541e0b2eeb00a8649bb8e189898d3072e34e8
JakobVokac <jakob.vokac@gmail.com>
2018-03-31 14:29:41 +0200

    fixed commented out code

--- zeeguu_api/api/dashboard.py
+++ zeeguu_api/api/dashboard.py
@@ -166,7 +166,7 @@ def link_teacher_class(user_id, cohort_id):
     return 'added teacher_class relationship'
 
 #Checks if the inputted invite code is already in use.
-#@api.route("/check_invite_code/<invite_code>", methods=["GET"])
+@api.route("/check_invite_code/<invite_code>", methods=["GET"])
 @with_session
 def check_inv_code(invite_code):
     c = Cohort.query.filter_by(inv_code=invite_code).first()
