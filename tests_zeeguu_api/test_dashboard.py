0d4d2f4899b31d0a0845f763f6f54514df315404
Oli26 <lolol1990@hotmail.co.uk>
2018-03-31 07:47:37 -0400

    changed remove class and get classes tests

--- tests_zeeguu_api/test_dashboard.py
+++ tests_zeeguu_api/test_dashboard.py
@@ -1,4 +1,3 @@
-
 import json
 from unittest import TestCase
 
@@ -7,27 +6,25 @@ from tests_zeeguu_api.api_test_mixin import APITestMixin
 import zeeguu
 from zeeguu_api.app import app
 
+
 class DashboardTest(APITestMixin, TestCase):
 
-
     def test_has_session(self):
-
         # TEST FOR FAKE SESSION
         result = self.app.get('/has_session?session=31235ds')
         result = json.loads(result.data)
-        assert result==0
+        assert result == 0
 
         # TEST FOR REAL SESSION
         userDictionary = {
-            'username':'testUser',
-            'password':'password'
+            'username': 'testUser',
+            'password': 'password'
         }
         rv = self.api_post('/add_user/test321@gmail.com', userDictionary)
         assert rv
-        result = self.app.get('/has_session?session='+rv.data.decode('utf-8'))
+        result = self.app.get('/has_session?session=' + rv.data.decode('utf-8'))
         result = json.loads(result.data)
-        assert result==1
-
+        assert result == 1
 
     def test_adding_classes(self):
         userDictionary = {
@@ -36,7 +33,6 @@ class DashboardTest(APITestMixin, TestCase):
         }
         rv = self.api_post('/add_user/test321@gmail.com', userDictionary)
         assert rv
-
 
         # Acceptable class
         classDictionary = {
@@ -48,7 +44,7 @@ class DashboardTest(APITestMixin, TestCase):
         result = self.app.post('/add_class?session=' + rv.data.decode('utf-8'), data=classDictionary)
         assert result.status_code == 200
         result = json.loads(result.data)
-        assert result==1
+        assert result == 1
 
         # Inv code aleady in use
         classDictionary = {
@@ -60,7 +56,6 @@ class DashboardTest(APITestMixin, TestCase):
         result = self.app.post('/add_class?session=' + rv.data.decode('utf-8'), data=classDictionary)
         assert result.status_code == 400
 
-
         # invalid max_students
         classDictionary = {
             'inv_code': '1234',
@@ -71,7 +66,7 @@ class DashboardTest(APITestMixin, TestCase):
         result = self.app.post('/add_class?session=' + rv.data.decode('utf-8'), data=classDictionary)
         assert result.status_code == 400
 
-        #invalid language_id
+        # invalid language_id
         classDictionary = {
             'inv_code': '12345',
             'class_name': 'FrenchB3',
@@ -81,10 +76,7 @@ class DashboardTest(APITestMixin, TestCase):
         result = self.app.post('/add_class?session=' + rv.data.decode('utf-8'), data=classDictionary)
         assert result.status_code == 400
 
-
-
-    def test_get_classes(self):
-
+    def test_get_classes_and_get_class_info(self):
         # Insert class from another teacher
         userDictionary = {
             'username': 'testUser1',
@@ -99,7 +91,6 @@ class DashboardTest(APITestMixin, TestCase):
         }
         result = self.app.post('/add_class?session=' + rv.data.decode('utf-8'), data=classDictionary)
 
-
         # Insert class from teacher we will test
         userDictionary = {
             'username': 'testUser2',
@@ -113,7 +104,7 @@ class DashboardTest(APITestMixin, TestCase):
             'max_students': '33'
         }
         result = self.app.post('/add_class?session=' + rv.data.decode('utf-8'), data=classDictionary)
-        classesJ = self.app.get('/get_classes?session='+rv.data.decode('utf-8'))
+        classesJ = self.app.get('/get_classes?session=' + rv.data.decode('utf-8'))
         assert classesJ
         classes = json.loads(classesJ.data)
         # Assert something is returned
@@ -122,8 +113,10 @@ class DashboardTest(APITestMixin, TestCase):
         # Assert the correct class of the two is returned
         assert classes[0]['class_name'] == 'FrenchB2'
 
-
-
+        # Test get class info
+        result = self.app.get('get_class_info/' + str(classes[0]['id']) + '?session=' + rv.data.decode('utf-8'))
+        assert result.status_code == 200
+        assert json.loads(result.data)['class_name'] == 'FrenchB2'
 
     def test_get_users_from_class(self):
         userDictionary = {
@@ -139,12 +132,11 @@ class DashboardTest(APITestMixin, TestCase):
         }
         result = self.app.post('/add_class?session=' + rv.data.decode('utf-8'), data=classDictionary)
 
-
         newUser = {
-            'username' : 'newUser1',
-            'password' : 'password',
-            'email' : 'newUser1@gmail.com',
-            'inv_code' : '12'
+            'username': 'newUser1',
+            'password': 'password',
+            'email': 'newUser1@gmail.com',
+            'inv_code': '12'
         }
         result = self.app.post('/add_user_with_class', data=newUser)
         assert result.status_code == 200
@@ -152,8 +144,6 @@ class DashboardTest(APITestMixin, TestCase):
         assert result.status_code == 200
         result = json.loads(result.data)
         assert result[0]['name'] == 'newUser1'
-
-
 
     def test_remove_class(self):
         userDictionary = {
@@ -169,5 +159,7 @@ class DashboardTest(APITestMixin, TestCase):
         }
         result = self.app.post('/add_class?session=' + rv.data.decode('utf-8'), data=classDictionary)
         assert result.status_code == 200
-        result = self.app.post('/remove_class/1?session='+ rv.data.decode('utf-8'))
+        result = self.app.post('/remove_class/1?session=' + rv.data.decode('utf-8'))
         assert result.status_code == 200
+        result = self.app.post('/remove_class/1?session=' + rv.data.decode('utf-8'))
+        assert result.status_code == 401
