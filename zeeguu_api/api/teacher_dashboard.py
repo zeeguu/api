3bf8762f049b0cb9f465664dade4c30f7c10b8af
Oli26 <lolol1990@hotmail.co.uk>
2018-04-23 13:44:13 -0400

    renaming

--- zeeguu_api/api/teacher_dashboard.py
+++ zeeguu_api/api/teacher_dashboard.py
@@ -214,17 +214,17 @@ def _link_teacher_cohort(user_id, cohort_id):
     return 'added teacher_cohort relationship'
 
 
-@api.route("/check_invite_code/<invite_code>", methods=["GET"])
+@api.route("/invite_code_exists/<invite_code>", methods=["GET"])
 @with_session
-def check_inv_code(invite_code):
+def inv_code_exists(invite_code):
     '''
         Checks if the inputted invite code is already in use.
 
     '''
     c = Cohort.query.filter_by(inv_code=invite_code).first()
     if c is None:
-        return "Taken"
-    return "OK"
+        return "OK"
+    return "False"
 
 
 @api.route("/create_own_cohort", methods=["POST"])
