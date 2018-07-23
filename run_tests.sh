08df16cf378047b296199b3b2b83995cd583b53c
Mircea Lungu <mircea.lungu@gmail.com>
2018-07-23 15:00:56 +0200

    running with python3 is better than 3.6

--- run_tests.sh
+++ run_tests.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 export PYTHONWARNINGS='ignore'
-python3.6 -m pytest --version 1>/dev/null 2>/dev/null || (echo "installing pytest..." && pip3.6 install pytest) && python3.6 -m pytest
+python3 -m pytest --version 1>/dev/null 2>/dev/null || (echo "installing pytest..." && pip3 install pytest) && python3 -m pytest
 export PYTHONWARNINGS='default'
