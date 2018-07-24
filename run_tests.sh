37666f7061ee5b1c4c1054fb92cd09f2c68b0b5e
Mircea Lungu <mircea.lungu@gmail.com>
2018-07-24 11:07:57 +0200

    switched back to using only python

--- run_tests.sh
+++ run_tests.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 export PYTHONWARNINGS='ignore'
-python3 -m pytest --version 1>/dev/null 2>/dev/null || (echo "installing pytest..." && pip3 install pytest) && python3 -m pytest
+python -m pytest --version 1>/dev/null 2>/dev/null || (echo "installing pytest..." && pip install pytest) && python -m pytest
 export PYTHONWARNINGS='default'
