debe7c64a9a38928c426ef386ac8f028688abc2b
Mircea Lungu <mircea.lungu@gmail.com>
2018-08-01 10:47:44 +0200

    user_article_history ... work in progress

--- zeeguu_api/api/user_articles.py
+++ zeeguu_api/api/user_articles.py
@@ -1,7 +1,7 @@
 import flask
 import zeeguu
 from zeeguu.content_recommender.mixed_recommender import article_recommendations_for_user
-from zeeguu.model import UserArticle
+from zeeguu.model import UserArticle, UserReadingSession, User
 
 from .utils.route_wrappers import cross_domain, with_session
 from .utils.json_result import json_result
@@ -32,3 +32,62 @@ def user_articles_recommended(count: int = 20):
 def user_articles_starred_and_liked():
     return json_result(UserArticle.all_starred_and_liked_articles_of_user_info(flask.g.user))
 
+
+# ---------------------------------------------------------------------------
+@api.route("/user_article_history/<user_id>", methods=("GET",))
+# ---------------------------------------------------------------------------
+@cross_domain
+# @with_session
+def user_article_history(user_id):
+    user = User.find_by_id(user_id)
+
+    sessions = UserReadingSession.find_by_user(user.id)
+
+    dates = {}
+    for each in sessions[:-650:-1]:
+        if each.article and each.duration > 1000:
+            if not dates.get(each.human_readable_date()):
+                dates[each.human_readable_date()] = []
+
+            # user_article = UserArticle.find(user, each.article)
+            events_in_this_session = each.events_in_this_session()
+
+            has_like = False
+            feedback = ""
+            difficulty = ""
+            for event in events_in_this_session:
+                if event.is_like():
+                    has_like = True
+                if event.is_feedback():
+                    feedback = event.extra_data
+                    difficulty = "fk: " + str(each.article.fk_difficulty)
+
+            dates[each.human_readable_date()].append(
+                {
+                    'date': each.human_readable_date(),
+                    'duration': each.human_readable_duration(),
+                    'start': each.start_time.strftime("%H:%M:%S"),
+                    'article': each.article.title,
+                    'liked': has_like,
+                    'feedback': feedback,
+                    'difficulty': difficulty
+                }
+            )
+
+    text_result = f"<title>{user.name}</title>"
+    text_result += f"<h1>{user.name} ({user.id})</h1><br/>"
+    previous_title = ""
+    for date in dates:
+        text_result += date + "<br/>"
+        for session in dates[date]:
+            if previous_title != session['article']:
+                text_result += f"<br/>&nbsp;&nbsp;<b> {session['article']} </b><br/>"
+            text_result += f"&nbsp;&nbsp;&nbsp;&nbsp; {session['duration']} ({session['start']})"
+            if session['liked']:
+                text_result += ' (LIKED) '
+            text_result += session['difficulty'] + " " + session['feedback'] + " <br/>"
+            previous_title = session['article']
+
+        text_result += "<br/><br/>"
+
+    return text_result
