f354158face91b8ac35bfe4a43ecd2d4711e8ba2
Mircea Lungu <mircea.lungu@gmail.com>
2018-08-01 22:12:58 +0200

    added article_id endpoint. changed all the user_activity and user_article endpoints to work with article_id as opposed to url.

--- zeeguu_api/user_activity_hooks/article_interaction_hooks.py
+++ zeeguu_api/user_activity_hooks/article_interaction_hooks.py
@@ -19,39 +19,31 @@ def distill_article_interactions(session, user, data):
     event = data['event']
     value = data['value']
     extra_data = data['extra_data']
+    article_id = int(data['article_id'])
 
     log(f'event is: {event}')
 
     if "UMR - OPEN ARTICLE" in event:
-        article_opened(session, value, user)
+        article_opened(session, article_id, user)
     elif "UMR - LIKE ARTICLE" in event:
-        article_liked(session, value, user, True)
+        article_liked(session, article_id, user, True)
     elif "UMR - UNLIKE ARTICLE" in event:
-        article_liked(session, value, user, False)
+        article_liked(session, article_id, user, False)
     elif "UMR - USER FEEDBACK" in event:
-        article_feedback(session, value, extra_data)
+        article_feedback(session, article_id, extra_data)
 
 
-def article_feedback(session, value, extra_data):
-    # the url that comes from zeeguu event logger
-    # might be the zeeguu url: which is of the form
-    # https://www.zeeguu.unibe.ch/read/article?articleLanguage=de&articleURL=https://www.nzz.ch/wissenschaft/neandertaler-waren-kuenstler-ld.1358862
-    # thus we extract only the last part
-    url = value.split('articleURL=')[-1]
-    article = Article.find_or_create(session, url)
+def article_feedback(session, article_id, extra_data):
+    article = Article.query.filter_by(id=article_id).one()
+
     if "not_finished_for_broken" in extra_data:
         article.vote_broken()
         session.add(article)
         session.commit()
 
-def article_liked(session, value, user, like_value):
-    # the url that comes from zeeguu event logger
-    # might be the zeeguu url: which is of the form
-    # https://www.zeeguu.unibe.ch/read/article?articleLanguage=de&articleURL=https://www.nzz.ch/wissenschaft/neandertaler-waren-kuenstler-ld.1358862
-    # thus we extract only the last part
-    url = value.split('articleURL=')[-1]
 
-    article = Article.find_or_create(session, url)
+def article_liked(session, article_id, user, like_value):
+    article = Article.query.filter_by(id=article_id).one()
     ua = UserArticle.find(user, article)
     ua.liked = like_value
     session.add(ua)
@@ -59,14 +51,8 @@ def article_liked(session, value, user, like_value):
     log(f"{ua}")
 
 
-def article_opened(session, value, user):
-    # the url that comes from zeeguu event logger
-    # might be the zeeguu url: which is of the form
-    # https://www.zeeguu.unibe.ch/read/article?articleLanguage=de&articleURL=https://www.nzz.ch/wissenschaft/neandertaler-waren-kuenstler-ld.1358862
-    # thus we extract only the last part
-    url = value.split('articleURL=')[-1]
-
-    article = Article.find_or_create(session, url)
+def article_opened(session, article_id, user):
+    article = Article.query.filter_by(id=article_id).one()
     ua = UserArticle.find(user, article)
     if not ua:
         ua = UserArticle.find_or_create(session, user, article, opened=datetime.now())
