24a73046c4a278cf2e8de9059699aaa4991a1096
Mircea Lungu <mircea.lungu@gmail.com>
2019-02-19 17:28:07 +0100

    working with icon_name instead of image_url

--- tools/add_rssfeed.py
+++ tools/add_rssfeed.py
@@ -5,7 +5,7 @@ import zeeguu_core
 
 
 def test_feed(url: str):
-    feed = RSSFeed.from_url(_feed_url)
+    feed = RSSFeed.from_url(url)
 
     feed_items = feed.feed_items()
     if not feed_items:
@@ -17,8 +17,6 @@ def test_feed(url: str):
     return feed
 
 
-RESOURCES_FOLDER = "https://zeeguu.unibe.ch/api/resources/"
-
 _feed_url = input("Feed url:  ")
 test_feed = test_feed(_feed_url)
 
@@ -27,7 +25,7 @@ print(f'= {feed_name}')
 
 icon_name = input(
     "Icon name to be found in resources folder (e.g. 20min.png):  ")
-print (f'= {icon_name}')
+print(f'= {icon_name}')
 
 description = input(f'Description (Enter for: {test_feed.description}): ') or test_feed.description
 print(f'= {description}')
@@ -35,11 +33,15 @@ print(f'= {description}')
 _language = input("Language code (e.g. en): ")
 print(f'= {_language}')
 
-icon_url = Url.find_or_create(zeeguu_core.db.session, RESOURCES_FOLDER + icon_name)
 feed_url = Url.find_or_create(zeeguu_core.db.session, _feed_url)
 language = Language.find_or_create(_language)
 
-rss_feed = RSSFeed.find_or_create(zeeguu_core.db.session, feed_url, feed_name, description, icon_url, language)
+rss_feed = RSSFeed.find_or_create(zeeguu_core.db.session,
+                                  feed_url,
+                                  feed_name,
+                                  description,
+                                  icon_name=icon_name,
+                                  language=language)
 
 print("Done: ")
 print(rss_feed.title)
