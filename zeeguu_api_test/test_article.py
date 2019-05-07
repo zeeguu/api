92736a7aba57bdf5a41ffc587a98f9a55d8bf405
Mircea Lungu <mircea.lungu@gmail.com>
2019-05-07 17:03:02 +0200

    added forgotten test; refactored the other one.

--- zeeguu_api_test/test_article.py
+++ zeeguu_api_test/test_article.py
@@ -1,5 +1,3 @@
-# coding=utf-8
-
 from unittest import TestCase
 
 from zeeguu_core_test.test_data.mocking_the_web import url_spiegel_venezuela
@@ -7,19 +5,12 @@ from zeeguu_api_test.api_test_mixin import APITestMixin
 import urllib.parse
 
 
-class MetaTests(APITestMixin, TestCase):
+class ArticleTests(APITestMixin, TestCase):
 
     def setUp(self):
-        super(MetaTests, self).setUp()
+        super(ArticleTests, self).setUp()
+        self.url_quoted = urllib.parse.quote_plus(url_spiegel_venezuela)
 
-    def test_routes_should_not_end_with_slash(self):
-
-        exceptions = ["/", "/dashboard/"]
-
-        for rule in self.app.application.url_map.iter_rules():
-            rule_name = rule.rule
-
-            if rule_name not in exceptions:
-                if rule_name[-1] == "/":
-                    print("\nOops! Rules should not end in /. \nOffending rule: " + rule.rule)
-                    assert (False)
+    def test_article_info_other_way(self):
+        result = self.json_from_api_get('/article_id', other_args=dict(url=self.url_quoted))
+        assert (result['article_id'])
