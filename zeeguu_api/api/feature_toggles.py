c4b616d67dd2b52e46a56b838945f9b875f26f24
Mircea Lungu <mircea.lungu@gmail.com>
2021-04-26 21:51:01 +0300

    a more flexible way of testing for a feature

--- zeeguu_api/api/feature_toggles.py
+++ zeeguu_api/api/feature_toggles.py
@@ -4,15 +4,25 @@ from zeeguu_api.api import api
 from zeeguu_api.api.utils.route_wrappers import cross_domain, with_session
 
 
-feature_map = {"activity_dashboard": [534, 2455, 2833]}
-
-
-@api.route("/is_feature_enabled/<feature_name>", methods=["POST"])
+@api.route("/is_feature_enabled/<feature_name>", methods=["GET"])
 @cross_domain
 @with_session
 def is_feature_enabled(feature_name):
 
-    if int(flask.g.user.id) in feature_map.get(feature_name, None):
-        return str("YES")
+    func = _feature_map().get(feature_name, None)
+
+    if not func:
+        return "NO"
+
+    if func(flask.g.user):
+        return "YES"
 
     return "NO"
+
+
+def _feature_map():
+    return {"activity_dashboard": _activity_dashboard_enabled}
+
+
+def _activity_dashboard_enabled(user):
+    return user.id in [534, 2455, 2833]
