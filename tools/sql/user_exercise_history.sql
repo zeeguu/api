2fe0064bffd6451ae8f9a53d0acbe65bb5c8c772
Mircea Lungu <mircea.lungu@gmail.com>
2021-05-01 19:23:01 +0300

    saving some queries in case they'll be needed later

---
+++ tools/sql/user_exercise_history.sql
@@ -0,0 +1,15 @@
+select b.user_id,  
+        e.time, 
+        uw.word, 
+        eo.outcome, 
+        es.source as ex_type
+from 
+    exercise as e
+    join bookmark_exercise_mapping as bem on bem.exercise_id = e.id
+    join bookmark as b on bem.bookmark_id = b.id
+    join user_word as uw on b.origin_id = uw.id
+    join exercise_outcome as eo on e.outcome_id = eo.id
+    join exercise_source as es on e.source_id = es.id
+
+where b.user_id = 62
+order by e.time desc
