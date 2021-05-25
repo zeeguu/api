c1cffe49be76b9d478308741ec388e25f94385f9
Mircea Lungu <mircea.lungu@gmail.com>
2021-05-25 14:20:08 +0200

    returning cohort_id

--- zeeguu_api/api/student.py
+++ zeeguu_api/api/student.py
@@ -3,22 +3,7 @@ from flask import request
 from zeeguu_core.model import Cohort
 
 from .utils.route_wrappers import cross_domain, with_session
-from . import api, db_session
-
-
-# @api.route("/join_cohort", methods=["POST"])
-# @with_session
-# def join_cohort():
-#
-#     invite_code = flask.request.form.get("invite_code")
-#
-#     cohort = Cohort.find_by_code(invite_code)
-#     flask.g.user.cohort_id = cohort.id
-#
-#     db.session.add(flask.g.user)
-#     db.session.commit()
-#
-#     return "OK"
+from . import api, db_session, json_result
 
 
 # ---------------------------------------------------------------------------
@@ -38,7 +23,8 @@ def join_cohort():
         db_session.add(flask.g.user)
         db_session.commit()
 
-        return cohort.id
+        return json_result({"cohort_id": cohort.id})
+
     except Exception as e:
         from sentry_sdk import capture_exception
 
