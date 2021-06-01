240111adedd4bb6c3cd211101bbd05a922d8d3bf
Mircea Lungu <mircea.lungu@gmail.com>
2021-06-01 16:59:54 +0300

    Update words_to_study.py

--- zeeguu_core/word_scheduling/arts/words_to_study.py
+++ zeeguu_core/word_scheduling/arts/words_to_study.py
@@ -8,24 +8,25 @@ from sqlalchemy import or_
 def bookmarks_to_study(user, desired_bookmarks_count=10):
     """
 
-        Returns a list of bookmarks with the highest priorities
-        An equal amount of bookmarks from each used algorithm (ABTesting) are selected
+    Returns a list of bookmarks with the highest priorities
+    An equal amount of bookmarks from each used algorithm (ABTesting) are selected
 
 
-        Otherwise, an equal amount of bookmarks is taken from each bookmark_group and concatenated into a list,
-        which is then returned. The amount of bookmarks taken from each group can differ by 1, depending on whether the
-        possible_bookmarks_to_return_count is equally dividable by the group count.
+    Otherwise, an equal amount of bookmarks is taken from each bookmark_group and concatenated into a list,
+    which is then returned. The amount of bookmarks taken from each group can differ by 1, depending on whether the
+    possible_bookmarks_to_return_count is equally dividable by the group count.
 
     """
-    bookmarks = (Bookmark.query.
-                 filter_by(user_id=user.id).
-                 filter_by(learned=False).
-                 join(BookmarkPriorityARTS, BookmarkPriorityARTS.bookmark_id == Bookmark.id).
-                 join(UserWord, Bookmark.origin_id == UserWord.id).
-                 filter(or_(Bookmark.fit_for_study == True, Bookmark.starred == True)).
-                 filter(UserWord.language_id == user.learned_language_id).
-                 order_by(BookmarkPriorityARTS.priority.desc()).
-                 all())
+    bookmarks = (
+        Bookmark.query.filter_by(user_id=user.id)
+        .filter_by(learned=False)
+        .filter(or_(Bookmark.fit_for_study == 1, Bookmark.starred == True))
+        .join(BookmarkPriorityARTS, BookmarkPriorityARTS.bookmark_id == Bookmark.id)
+        .join(UserWord, Bookmark.origin_id == UserWord.id)
+        .filter(UserWord.language_id == user.learned_language_id)
+        .order_by(BookmarkPriorityARTS.priority.desc())
+        .all()
+    )
 
     # Group the bookmarks by their used priority algorithm in lists
     bookmark_groups = ABTesting.split_bookmarks_based_on_algorithm(bookmarks)
