b6d7e55bc4e347cbd38ef043cb26299ed94c66e1
Mircea Lungu <mircea.lungu@gmail.com>
2021-06-26 10:03:44 +0200

    removing migrations; nobody ever wants to see them; reorganizing the sql folder

--- tools/sql/migrations/empty_word_map.sql
+++
@@ -1,3 +0,0 @@
-DELETE FROM article_word_map;
-DELETE FROM article_word;
-ALTER TABLE article_word AUTO_INCREMENT = 1;
--- tools/sql/migrations/wordUnique.sql
+++
@@ -1,9 +0,0 @@
--- This script is executed to make word and language_id column to unique in ranked_word and user_word
-
-ALTER TABLE ranked_word
-DROP INDEX uc_wordID,
-ADD CONSTRAINT wr_word UNIQUE (word,language_id);
-
-ALTER TABLE user_word
-DROP INDEX uc_wordID,
-ADD CONSTRAINT uw_word UNIQUE (word,language_id)
