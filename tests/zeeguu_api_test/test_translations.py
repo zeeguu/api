cc9501ac6d7ce57ec1b5a4bad57477d530e399a2
Mircea Lungu <mircea.lungu@gmail.com>
2021-07-12 20:07:55 +0200

    removed unused endpoints and corresponding tests

--- tests/zeeguu_api_test/test_translations.py
+++ tests/zeeguu_api_test/test_translations.py
@@ -1,10 +1,7 @@
 # coding=utf-8
 from unittest import TestCase
-from unittest.mock import patch, call
 
-from zeeguu_api.api.translator import minimize_context
 from zeeguu_api_test.api_test_mixin import APITestMixin
-from zeeguu_api_test.mocks.mock_translator import MockTranslator
 from zeeguu_core_test.test_data.mocking_the_web import url_spiegel_venezuela
 
 
@@ -26,137 +23,6 @@ class TranslationTests(APITestMixin, TestCase):
             FROM_LANG_CODE,
             TO_LANG_CODE,
         )
-
-    @patch("zeeguu_api.api.translate_and_bookmark.get_all_translations")
-    def test_get_possible_translations(self, mock_get_all_translations):
-        self.data["context"] = "Die klein Jäger"
-        self.data["word"] = "klein"
-        MIN_CONTEXT, self.data["query"] = minimize_context(
-            self.data["context"], self.data["from_lang_code"], self.data["word"]
-        )
-        mock_get_all_translations.return_value = MockTranslator(
-            {"klein": ["small", "little"], "krassen": ["big"]}
-        ).translate(self.data["query"])
-
-        form_data = dict(
-            url=self.data["url"], context=self.data["context"], word=self.data["word"]
-        )
-        with patch("zeeguu_api.api.translate_and_bookmark.minimize_context") as (
-            mock_minimize_context
-        ):
-            mock_minimize_context.return_value = (MIN_CONTEXT, self.data["query"])
-            alternatives = self.json_from_api_post(self.api_endpoint, form_data)
-            mock_minimize_context.assert_called_once_with(
-                self.data["context"], self.data["from_lang_code"], self.data["word"]
-            )
-
-        first_alternative = alternatives["translations"][0]
-        second_alternative = alternatives["translations"][1]
-
-        mock_get_all_translations.assert_called_once_with(self.data)
-        assert first_alternative is not None
-        assert second_alternative is not None
-        assert first_alternative["likelihood"] >= second_alternative["likelihood"]
-
-    @patch("zeeguu_api.api.translate_and_bookmark.get_all_translations")
-    def test_get_translation_where_gslobe_fails_but_translate_succeeds(
-        self, mock_get_all_translations
-    ):
-        self.data["context"] = "Die klein Jäger"
-        self.data["word"] = "krassen"
-        MIN_CONTEXT, self.data["query"] = minimize_context(
-            self.data["context"], self.data["from_lang_code"], self.data["word"]
-        )
-        mock_get_all_translations.return_value = MockTranslator(
-            {"klein": ["small"], "krassen": ["big", "extreme"]}
-        ).translate(self.data["query"])
-
-        form_data = dict(
-            url=self.data["url"], context=self.data["context"], word=self.data["word"]
-        )
-        with patch("zeeguu_api.api.translate_and_bookmark.minimize_context") as (
-            mock_minimize_context
-        ):
-            mock_minimize_context.return_value = (MIN_CONTEXT, self.data["query"])
-            alternatives = self.json_from_api_post(self.api_endpoint, form_data)
-            mock_minimize_context.assert_called_once_with(
-                self.data["context"], self.data["from_lang_code"], self.data["word"]
-            )
-
-        mock_get_all_translations.assert_called_once_with(self.data)
-
-        first_alternative = alternatives["translations"][0]
-        assert first_alternative is not None
-
-        second_alternative = alternatives["translations"][1]
-        assert second_alternative is not None
-
-    @patch("zeeguu_api.api.translate_and_bookmark.get_all_translations")
-    def test_translate_and_bookmark(self, mock_get_all_translations):
-        self.data["context"] = "Die klein Jäger"
-        self.data["word"] = "Die"
-        MIN_CONTEXT, self.data["query"] = minimize_context(
-            self.data["context"], self.data["from_lang_code"], self.data["word"]
-        )
-        new_data = self.data.copy()
-        new_data["word"] = "kleine"
-        NEW_MIN_CONTEXT, new_data["query"] = minimize_context(
-            new_data["context"], new_data["from_lang_code"], new_data["word"]
-        )
-        mock_translator = MockTranslator({"Die": ["The"], "kleine": ["small"]})
-        new_mock_translator = MockTranslator({"Die": ["The"], "kleine": ["small"]})
-        # bookmark1 call will modify the object, we need to return a copy of
-        # the same object for bookmark2 call
-        mock_get_all_translations.side_effect = [
-            mock_translator.translate(self.data["query"]),
-            mock_translator.translate(self.data["query"]),
-            new_mock_translator.translate(new_data["query"]),
-        ]
-
-        form_data = dict(
-            url=self.data["url"], context=self.data["context"], word=self.data["word"]
-        )
-
-        with patch("zeeguu_api.api.translate_and_bookmark.minimize_context") as (
-            mock_minimize_context
-        ):
-            mock_minimize_context.return_value = (MIN_CONTEXT, self.data["query"])
-            bookmark1 = self.json_from_api_post(
-                "/translate_and_bookmark/%s/%s"
-                % (self.data["from_lang_code"], self.data["to_lang_code"]),
-                form_data,
-            )
-            bookmark2 = self.json_from_api_post(
-                "/translate_and_bookmark/%s/%s"
-                % (self.data["from_lang_code"], self.data["to_lang_code"]),
-                form_data,
-            )
-            calls = [
-                call(
-                    self.data["context"], self.data["from_lang_code"], self.data["word"]
-                )
-            ] * 2
-            mock_minimize_context.assert_has_calls(calls)
-
-        assert bookmark1["bookmark_id"] == bookmark2["bookmark_id"]
-
-        form_data["word"] = new_data["word"]
-        with patch("zeeguu_api.api.translate_and_bookmark.minimize_context") as (
-            mock_minimize_context
-        ):
-            mock_minimize_context.return_value = (NEW_MIN_CONTEXT, new_data["query"])
-            bookmark3 = self.json_from_api_post(
-                "/translate_and_bookmark/%s/%s"
-                % (self.data["from_lang_code"], self.data["to_lang_code"]),
-                form_data,
-            )
-            mock_minimize_context.assert_called_once_with(
-                new_data["context"], new_data["from_lang_code"], new_data["word"]
-            )
-
-        calls = [call(self.data), call(self.data), call(new_data)]
-        mock_get_all_translations.assert_has_calls(calls)
-        self.assertTrue(bookmark3["translation"] == "small")
 
     def test_minimize_context(self):
         from zeeguu_api.api.translate_and_bookmark import minimize_context
--- zeeguu_api/api/translate_and_bookmark.py
+++ zeeguu_api/api/translate_and_bookmark.py
@@ -1,105 +1,28 @@
-import datetime
-
-from sqlalchemy.orm.exc import NoResultFound
 from urllib.parse import unquote_plus
 
 import flask
 from flask import request
+from sqlalchemy.orm.exc import NoResultFound
 
 import zeeguu_core
+from zeeguu_api.api.translator import (
+    minimize_context,
+    get_next_results,
+    contribute_trans,
+)
 from zeeguu_core.crowd_translations import (
     own_or_crowdsourced_translation,
     own_translation,
 )
-
-from . import api, db_session
-from .utils.route_wrappers import cross_domain, with_session
-from .utils.json_result import json_result
 from zeeguu_core.model import (
     Bookmark,
     Article,
     ExerciseSource,
     ExerciseOutcome,
-    Exercise,
 )
-from zeeguu_api.api.translator import (
-    minimize_context,
-    get_all_translations,
-    get_next_results,
-    contribute_trans,
-)
-
-
-@api.route(
-    "/get_possible_translations/<from_lang_code>/<to_lang_code>", methods=["POST"]
-)
-@cross_domain
-@with_session
-def get_possible_translations(from_lang_code, to_lang_code):
-    """
-
-    Returns a list of possible translations in :param to_lang_code
-    for :param word in :param from_lang_code.
-
-    You must also specify the :param context, :param url, and :param title
-     of the page where the word was found.
-
-    The context is the sentence.
-
-    :return: json array with translations
-
-    """
-    data = {"from_lang_code": from_lang_code, "to_lang_code": to_lang_code}
-    data["context"] = request.form.get("context", "")
-    url = request.form.get("url", "")
-    data["url"] = url
-    article_id = None
-    if "articleID" in url:
-        article_id = url.split("articleID=")[-1]
-        url = Article.query.filter_by(id=article_id).one().url.as_canonical_string()
-    elif "articleURL" in url:
-        url = url.split("articleURL=")[-1]
-    else:
-        # the url comes from elsewhere not from the reader, so we find or creat the article
-        article = Article.find_or_create(db_session, url)
-        article_id = article.id
-    zeeguu_core.log(f"url before being saved: {url}")
-    word_str = request.form["word"]
-    data["word"] = word_str
-    title_str = request.form.get("title", "")
-    data["title"] = title_str
-
-    zeeguu_core.log(f'translating to... {data["to_lang_code"]}')
-    minimal_context, query = minimize_context(
-        data["context"], data["from_lang_code"], data["word"]
-    )
-    zeeguu_core.log(f"Query to translate is: {query}")
-    data["query"] = query
-    translations = get_all_translations(data).translations
-    zeeguu_core.log(f"Got translations: {translations}")
-
-    # translators talk about quality, but our users expect likelihood.
-    # rename the key in the dictionary
-    for t in translations:
-        t["likelihood"] = t.pop("quality")
-        t["source"] = t.pop("service_name")
-
-    best_guess = translations[0]["translation"]
-
-    Bookmark.find_or_create(
-        db_session,
-        flask.g.user,
-        word_str,
-        from_lang_code,
-        best_guess,
-        to_lang_code,
-        minimal_context,
-        url,
-        title_str,
-        article_id,
-    )
-
-    return json_result(dict(translations=translations))
+from . import api, db_session
+from .utils.json_result import json_result
+from .utils.route_wrappers import cross_domain, with_session
 
 
 @api.route("/get_next_translations/<from_lang_code>/<to_lang_code>", methods=["POST"])
@@ -409,77 +332,3 @@ def unstar_bookmark(bookmark_id):
     bookmark.update_fit_for_study()
     db_session.commit()
     return "OK"
-
-
-# --- DANGER ZONE: Deprecated Endpoint --- #
-# ---------------------------------------- #
-
-
-@api.route("/translate_and_bookmark/<from_lang_code>/<to_lang_code>", methods=["POST"])
-@cross_domain
-@with_session
-def translate_and_bookmark(from_lang_code, to_lang_code):
-    """
-
-        @deprecated
-        This should be deprecated and /get_possible_translations used instead
-        However, it is still used by the zeeguu chrome extension.
-
-        This expects in the post parameter the following:
-        - word (to translate)
-        - context (surrounding paragraph of the original word )
-        - url (of the origin)
-        - title (of the origin page)
-
-        /get_possible_translations has very similar behavior, only that
-          if focuses on returning the possible alternative translations
-
-    :param from_lang_code:
-    :param to_lang_code:
-    :return:
-    """
-
-    data = {"from_lang_code": from_lang_code, "to_lang_code": to_lang_code}
-    word_str = unquote_plus(request.form["word"])
-    data["word"] = word_str
-    url_str = request.form.get("url", "")
-    data["url"] = url_str
-
-    title_str = request.form.get("title", "")
-    data["title"] = title_str
-    context_str = request.form.get("context", "")
-    data["context"] = context_str
-
-    # the url comes from elsewhere not from the reader, so we find or creat the article
-    article = Article.find_or_create(db_session, url_str)
-    article_id = article.id
-
-    try:
-
-        minimal_context, query = minimize_context(
-            data["context"], data["from_lang_code"], data["word"]
-        )
-        data["query"] = query
-        translations = get_all_translations(data).translations
-
-        best_guess = translations[0]["translation"]
-
-        bookmark = Bookmark.find_or_create(
-            db_session,
-            flask.g.user,
-            word_str,
-            from_lang_code,
-            best_guess,
-            to_lang_code,
-            minimal_context,
-            url_str,
-            title_str,
-            article_id,
-        )
-    except ValueError as e:
-        zeeguu_core.log(
-            f"minimize context failed {e}on: {context_str} x {from_lang_code} x {word_str} "
-        )
-        return context_str, query
-
-    return json_result(dict(bookmark_id=bookmark.id, translation=best_guess))
