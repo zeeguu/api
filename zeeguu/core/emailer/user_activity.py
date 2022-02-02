f1fd1c14d9c6f82f619b904131a6b49823769600
Mircea Lungu <mircea.lungu@gmail.com>
2022-02-02 19:44:12 +0100

    Update user_activity.py

--- zeeguu/core/emailer/user_activity.py
+++ zeeguu/core/emailer/user_activity.py
@@ -1,25 +1,34 @@
 from zeeguu.core.model import User
 from zeeguu.core.emailer.zeeguu_mailer import ZeeguuMailer
 
-cheers_your_server = '\n\rCheers,\n\rYour Zeeguu Server ;)'
+cheers_your_server = "\n\rCheers,\n\rYour Zeeguu Server ;)"
 
 
-def send_new_user_account_email(username, invite_code='', cohort=''):
+def send_new_user_account_email(username, invite_code="", cohort=""):
     ZeeguuMailer.send_mail(
-        f'New Account: {username}',
-        [
-            f'Code: {invite_code} Class: {cohort}',
-            cheers_your_server
-        ])
+        f"New Account: {username}",
+        [f"Code: {invite_code} Class: {cohort}", cheers_your_server],
+    )
 
 
-def send_notification_article_feedback(feedback, user: User, article_title, article_url, article_id):
-    cohort_id = user.cohort_id or 0
+def send_notification_article_feedback(
+    feedback, user: User, article_title, article_url, article_id
+):
+    def detailed_article_info(user, article_id):
+        info = "==Translations\n\n"
 
-    content_lines = [
-        f'{feedback}',
-        f'User Translations: https://www.zeeguu.org/bookmarks_for_article/{article_id}/{user.id}',
-        cheers_your_server
-    ]
+        bookmarks = user.bookmarks_for_article(
+            article_id, with_context=True, with_title=True
+        )
 
-    ZeeguuMailer.send_mail(f'{user.name} - {article_title}', content_lines)
+        for each in bookmarks:
+            info += f"{each.origin.word} = {each.translation.word}"
+
+        return info
+
+    content_lines = feedback
+    content_lines += detailed_article_info(user, article_id)
+    content_lines += f"Detailed User Translations: https://www.zeeguu.org/bookmarks_for_article/{article_id}/{user.id}"
+    content_lines += cheers_your_server
+
+    ZeeguuMailer.send_mail(f"{user.name} - {article_title}", content_lines)
