7a1adb2c73003780135b5f26e4aa28f1ff8abbe8
Mircea Lungu <mircea.lungu@gmail.com>
2022-02-02 19:54:14 +0100

    Update user_activity.py

--- zeeguu/core/emailer/user_activity.py
+++ zeeguu/core/emailer/user_activity.py
@@ -14,21 +14,24 @@ def send_new_user_account_email(username, invite_code="", cohort=""):
 def send_notification_article_feedback(
     feedback, user: User, article_title, article_url, article_id
 ):
-    def detailed_article_info(user, article_id):
-        info = "==Translations\n\n"
+    def detailed_article_info(stream, user, article_id):
+        stream.append("==Translations\n\n")
 
         bookmarks = user.bookmarks_for_article(
             article_id, with_context=True, with_title=True, json=False
         )
 
         for each in bookmarks:
-            info += f"{each.origin.word} = {each.translation.word}"
+            stream.append(f"{each.origin.word} = {each.translation.word}")
 
         return info
 
-    content_lines = feedback
-    content_lines += detailed_article_info(user, article_id)
-    content_lines += f"Detailed User Translations: https://www.zeeguu.org/bookmarks_for_article/{article_id}/{user.id}"
-    content_lines += cheers_your_server
+    content_lines = [feedback]
+    detailed_article_info(content_lines, user, article_id)
+
+    content_lines.append(
+        f"Detailed User Translations: https://www.zeeguu.org/bookmarks_for_article/{article_id}/{user.id}"
+    )
+    content_lines.append(cheers_your_server)
 
     ZeeguuMailer.send_mail(f"{user.name} - {article_title}", content_lines)
