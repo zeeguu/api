d02860f24e69c658e947b3e74ee17466a20277b4
Mircea Lungu <mircea.lungu@gmail.com>
2022-05-19 17:51:35 +0300

    renamed rank_id to rank

--- zeeguu/core/sql/queries/words_to_study.sql
+++ zeeguu/core/sql/queries/words_to_study.sql
@@ -1,4 +1,4 @@
-select b.id bookmark_id, b.starred , uw.word, tw.word , uw.rank_id , b.learned , b.fit_for_study , b.learned_time, bss.id 
+select b.id bookmark_id, b.starred , uw.word, tw.word , uw.rank , b.learned , b.fit_for_study , b.learned_time, bss.id 
 from bookmark b
 join user u on u.id = b.user_id
 join user_word uw on b.origin_id  = uw.id
@@ -13,6 +13,6 @@ and bss.id is null
 and u.id = :user_id
 and uw.language_id = :language_id
 
-order by b.starred desc, uw.rank_id
+order by b.starred desc, uw.rank is null, uw.rank asc
 
 limit :required_count
