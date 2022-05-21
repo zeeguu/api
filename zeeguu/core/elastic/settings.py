855b3337b8d8c63279ca774b9f81a38b23efda1a
Mircea Lungu <mircea.lungu@gmail.com>
2022-05-21 19:18:53 +0300

    Update settings.py

--- zeeguu/core/elastic/settings.py
+++ zeeguu/core/elastic/settings.py
@@ -3,7 +3,7 @@ import os
 
 # Example of a elastic connection string: USERNAME:PASSWORD@127.0.0.1:9200
 # if working without user and password, '127.0.0.1:9200' will suffice
-ES_CONN_STRING = os.environ.get("ZEEGUU_ES_CONN_STRING", "127.0.0.1:9200")
+ES_CONN_STRING = os.environ.get("ZEEGUU_ES_CONN_STRING", "http://127.0.0.1:9200")
 
 # what index to use in elasticsearch
 ES_ZINDEX = "zeeguu"
