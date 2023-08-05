bd91dd9da35f8fbfb6b625af86ecd64353437e85
Mircea Filip Lungu <mircea.lungu@gmail.com>
2023-08-05 17:12:28 +0300

    added a simple anonimized test tool

--- tools/anonymize_users.py
+++ tools/anonymize_users.py
@@ -10,21 +10,22 @@ import sqlalchemy
 
 import zeeguu.core
 from faker import Faker
+
 fake = Faker()
 from zeeguu.core.model import User
 
 session = zeeguu.core.db.session
 
 for user in User.query.all():
-    for _ in range(0,13):
+    for _ in range(0, 13):
         try:
             user.name = fake.name()
             user.email = fake.email()
             session.add(user)
             session.commit()
-            print (f"anonimized user id {user.id} to {user.name}")
+            print(f"anonymized user id {user.id} to {user.name}")
             break
         except sqlalchemy.exc.IntegrityError as e:
             session.rollback()
-            print (f"retrying...")
+            print(f"retrying...")
             continue
