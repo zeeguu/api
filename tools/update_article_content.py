ca10bbfc1f803e43cd718062476fbd3065823153
Mircea Filip Lungu <mircea.lungu@gmail.com>
2023-08-09 17:36:22 +0300

    updated script

--- tools/update_article_content.py
+++ tools/update_article_content.py
@@ -7,7 +7,11 @@ session = zeeguu.core.db.session
 
 def update_article(id):
     a = Article.find_by_id(id)
+    print(">>>>>> BEFORE <<<<<<")
+    print(a.content)
     a.update_content(session)
+    print("\n\n>>>>>> AFTER <<<<<<\n")
+    print(a.content)
 
 
 if __name__ == '__main__':
