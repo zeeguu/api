ac386a96da652d9e8a240da075d0b14c900fcddb
Mircea Lungu <mircea.lungu@gmail.com>
2023-09-04 17:14:17 +0300

    mail notifications for content update

--- tools/update_article_content.py
+++ tools/update_article_content.py
@@ -3,6 +3,9 @@ import zeeguu
 from zeeguu.api.app import app
 from zeeguu.core.model import Article
 from zeeguu.core.emailer.zeeguu_mailer import ZeeguuMailer
+from random import randint
+from time import sleep
+
 
 session = zeeguu.core.db.session
 
@@ -16,6 +19,13 @@ def update_article(id):
     print("\n\n>>>>>> AFTER <<<<<<\n")
     print(a.content)
     ZeeguuMailer.send_content_retrieved_notification(a, old_content)
+
+
+def update_article_range(start, end):
+    for i in range(start, end):
+        update_article(i)
+        sleep(randint(10, 100))
+
 
 if __name__ == '__main__':
     id = int(sys.argv[1])
