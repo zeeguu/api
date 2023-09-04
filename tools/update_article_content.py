c7b9180f6911d2b7ff2acb58b6deb0a425ac33ee
Mircea Lungu <mircea.lungu@gmail.com>
2023-09-04 13:06:05 +0300

    mail notifications for content update

--- tools/update_article_content.py
+++ tools/update_article_content.py
@@ -1,6 +1,8 @@
 import sys
 import zeeguu
 from zeeguu.core.model import Article
+from zeeguu.core.emailer.zeeguu_mailer import ZeeguuMailer
+
 
 session = zeeguu.core.db.session
 
@@ -12,6 +14,14 @@ def update_article(id):
     a.update_content(session)
     print("\n\n>>>>>> AFTER <<<<<<\n")
     print(a.content)
+
+    title = f"Updated Content for article {a.id}"
+    content = f"https://www.zeeguu.org/read/article?id={a.id}"
+    content += a.title + "\n"
+    content += a.content
+
+    ZeeguuMailer.send_mail(title, content, "mircea.lungu@gmail.com")
+
 
 
 if __name__ == '__main__':
