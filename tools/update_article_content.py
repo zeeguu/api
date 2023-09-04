14b568f568595750907ae7a5e8ac785ab22cb82a
Mircea Lungu <mircea.lungu@gmail.com>
2023-09-04 18:04:31 +0300

    mail notifications for content update

--- tools/update_article_content.py
+++ tools/update_article_content.py
@@ -5,7 +5,7 @@ from zeeguu.core.model import Article
 from zeeguu.core.emailer.zeeguu_mailer import ZeeguuMailer
 from random import randint
 from time import sleep
-
+from sqlalchemy import desc
 
 session = zeeguu.core.db.session
 
@@ -21,9 +21,12 @@ def update_article(id):
     ZeeguuMailer.send_content_retrieved_notification(a, old_content)
 
 
-def update_article_range(start, end):
-    for i in range(start, end):
-        update_article(i)
+# fr = 7
+def update_article_range(start_date, end_date, language_id):
+    all = Article.query.filter(Article.language_id == language_id).filter(Article.published_time >= start_date).filter(
+        Article.published_time < end_date).order_by(desc(Article.id)).all()
+    for each in all:
+        update_article(each.id)
         sleep(randint(10, 70))
 
 
