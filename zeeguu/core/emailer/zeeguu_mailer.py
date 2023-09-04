f1fa424d6b97c448c213efdacfd7f8c49f1abf1b
Mircea Lungu <mircea.lungu@gmail.com>
2023-09-04 18:26:45 +0300

    mail notifications for content update

--- zeeguu/core/emailer/zeeguu_mailer.py
+++ zeeguu/core/emailer/zeeguu_mailer.py
@@ -101,9 +101,9 @@ class ZeeguuMailer(object):
     def send_content_retrieved_notification(cls, a, old_content):
         title = f"{a.title} [Update]"
         content = f"https://www.zeeguu.org/read/article?id={a.id}" + "\n\n"
-        content += f"Original: {a.url.as_string()}"
-        content += f"Difficulty: {a.fk_difficulty}"
-        content += f"Word Count: {a.word_count}"
+        content += f"Original: {a.url.as_string()}"+ "\n"
+        content += f"Difficulty: {a.fk_difficulty}"+ "\n"
+        content += f"Word Count: {a.word_count}"+ "\n"
         content += "\n\n" + a.title + "\n\n"
         content += a.content
         content += "--------" + "\n"
