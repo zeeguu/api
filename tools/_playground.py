95888107dd183db6af91c6629d0f34311661f47b
Mircea Filip Lungu <mircea.lungu@gmail.com>
2023-10-04 17:47:58 -0500

    extending the exercise timeout duration to 90s

--- tools/_playground.py
+++ tools/_playground.py
@@ -1,8 +1,15 @@
-from zeeguu.core.model import UserExerciseSession
+from zeeguu.core.model import UserExerciseSession, User
 from zeeguu.api.app import create_app
+from zeeguu.core.user_statistics.activity import exercises_duration_by_day
 
 app = create_app()
 app.app_context().push()
-s = UserExerciseSession.query.filter_by(id=45768).one()
-exercises = s.exercises_in_session_string()
-print(exercises)
+
+print("before the for")
+for id in User.all_recent_user_ids(150):
+    u = User.find_by_id(id)
+    print(u.name)
+    duration_old = exercises_duration_by_day(u)
+    duration_new = exercises_duration_by_day(u, False)
+    print(duration_old)
+    print(duration_new)
