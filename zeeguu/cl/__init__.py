6af9bd53716994f7dfe950c606166dfa6190d1f0
Mircea Filip Lungu <mircea.lungu@gmail.com>
2023-10-16 17:20:10 +0200

    added zeeguu.cl for commandline tools

--- zeeguu/cl/__init__.py
+++ zeeguu/cl/__init__.py
@@ -1,5 +1,7 @@
-# CL = Command Line
-# Utility package that creates an app and pushes its context such that tools can be run
+# zeeguu.cl = Zeeguu Command Line
+# Package that creates an app and pushes its context such that tools can be run
+# The alternative would be to have run the three lines below every time one wanted to
+# run a tool or interact with the zeeguu from the REPL
 
 from zeeguu.api.app import create_app
 
