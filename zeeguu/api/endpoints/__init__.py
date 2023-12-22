88b5b8e18340814d846c13371707ba7dbec9ff69
Mircea Filip Lungu <mircea.lungu@gmail.com>
2023-12-22 15:38:54 +0200

    typo

--- zeeguu/api/endpoints/__init__.py
+++ zeeguu/api/endpoints/__init__.py
@@ -4,7 +4,7 @@ import zeeguu.core
 api = flask.Blueprint("endpoints", __name__)
 db_session = zeeguu.core.model.db.session
 
-print("loading endpoints endpionts...")
+print("loading endpoints...")
 
 # These files have to be imported after this line;
 # They enrich the endpoints object
