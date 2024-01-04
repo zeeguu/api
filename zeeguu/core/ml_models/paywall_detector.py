52600f7b34e3aea99f1d23a30d91c7945dcf1969
Tiago Ribeiro <tfnribeiro@gmail.com>
2024-01-04 09:33:57 +0100

    Update paywall_detector.py
    - Removed print statement to detect language

--- zeeguu/core/ml_models/paywall_detector.py
+++ zeeguu/core/ml_models/paywall_detector.py
@@ -4,5 +4,5 @@ from .utils import pre_process
 
 def is_paywalled(article_txt:str, model):
     lang = detect(article_txt)
-    print("Language detected was: ", lang)
+    #print("Language detected was: ", lang)
     return model.predict([pre_process(article_txt, lang)])
