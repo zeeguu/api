17d7f30e4081979ae9e45d24b5f569bcf3fbcebd
Tiago Ribeiro <tfnribeiro@gmail.com>
2024-01-29 10:55:28 +0100

    updated the SQL to match the changes

---
+++ tools/migrations/rss_to_feed.sql
@@ -0,0 +1,17 @@
+/* Update the table to be feed rather than rss_feed.
+  Add the column feed_type which should match the handlers defined in 
+    zeeguu/core/feedhandler.py
+*/
+ALTER TABLE `zeeguu_test`.`rss_feed` 
+ADD COLUMN `feed_type` INT NOT NULL DEFAULT 0 AFTER `deactivated`, RENAME TO  `zeeguu_test`.`feed` ;
+
+/* Update the article table to match the updated feed table.
+*/
+ALTER TABLE `zeeguu_test`.`article` 
+DROP FOREIGN KEY `article_ibfk_1`;
+ALTER TABLE `zeeguu_test`.`article` 
+CHANGE COLUMN `rssfeed_id` `feed_id` INT NULL DEFAULT NULL ;
+ALTER TABLE `zeeguu_test`.`article` 
+ADD CONSTRAINT `article_ibfk_1`
+  FOREIGN KEY (`feed_id`)
+  REFERENCES `zeeguu_test`.`feed` (`id`);
