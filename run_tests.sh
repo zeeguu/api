33114307c322e473c6c22bc596cf3b305fedfb44
nselpriv <nicolaiselo@gmail.com>
2024-01-30 15:43:15 +0100

    tests changed to run, sometimes..

--- run_tests.sh
+++ run_tests.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 export PYTHONWARNINGS='ignore'
-python -m pytest --version 1>/dev/null 2>/dev/null || (echo "installing pytest..." && pip install pytest) && python -m pytest
+pip install pytest ||
+python3 -m pytest #--version 1>/dev/null 2>/dev/null || (echo "installing pytest..." && pip install pytest) && python3 -m pytest
 ret_code=$?
 export PYTHONWARNINGS='default'
 exit $ret_code
