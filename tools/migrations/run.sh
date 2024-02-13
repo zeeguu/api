18c54e94bc148a4fd7d30b4ed34338a6aea6a5bf
Mircea Filip Lungu <mircea.lungu@gmail.com>
2024-02-13 14:42:44 +0200

    Update run.sh

--- tools/migrations/run.sh
+++ tools/migrations/run.sh
@@ -2,6 +2,6 @@ set -u # or set -o nounset
 
 # Example of use
 # ./run.sh 24-02-rss_to_feed.sql
-
+# Script expects the four variables below to be defined
 
 mysql -h $ZEEGUU_MYSQL_HOST -u $ZEEGUU_MYSQL_USER -p$ZEEGUU_MYSQL_PASS $ZEEGUU_MYSQL_DB < $1
