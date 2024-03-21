9d9650b45b8cd923d4fc03213cbe61ccc8873be5
Mircea Filip Lungu <mircea.lungu@gmail.com>
2024-03-21 11:05:57 +0200

    removing this because the RSSFeed#get_feed_articles blocks, even after adding the requests.get(self.url, headers=headers, timeout=(connect_timeout_seconds, read_timeout_seconds))...

--- tools/rename_feed.py
+++ tools/rename_feed.py
@@ -13,7 +13,6 @@ feed = Feed.find_by_id(feed_id)
 print(feed)
 print(f"Found feed with title: {feed.title} and url: ")
 print(feed.url.as_string())
-print(feed.feed_health_info())
 
 new_url = input("Input new feed url: ")
 url_object = Url.find_or_create(db_session, new_url, feed.title)
