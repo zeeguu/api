6c16ca3946c68857d7bf206fbd03512ef22a3f77
Mircea Filip Lungu <mircea.lungu@gmail.com>
2024-03-21 21:39:20 +0200

    simpler playground

--- tools/_playground.py
+++ tools/_playground.py
@@ -1,5 +1,4 @@
 from zeeguu.core.model import User
-from zeeguu.core.user_statistics.activity import exercises_duration_by_day
 
 from zeeguu.api.app import create_app
 
@@ -7,13 +6,6 @@ app = create_app()
 app.app_context().push()
 
 print("before the for")
-for id in User.all_recent_user_ids(150):
+for id in User.all_recent_user_ids(10):
     u = User.find_by_id(id)
     print(u.name)
-    duration_old = exercises_duration_by_day(u, True)
-    duration_new = exercises_duration_by_day(u)
-    if duration_new != duration_old:
-        print("old way")
-        print(duration_old)
-        print("new way")
-        print(duration_new)
