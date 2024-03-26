b475052f02469cf73dfad2129f704dadc8140ef1
Mircea Filip Lungu <mircea.lungu@gmail.com>
2024-03-26 10:29:33 +0200

    made the crawler more robust to situations where the session needs to be rolled back.

--- tools/feed_retrieval.py
+++ tools/feed_retrieval.py
@@ -18,8 +18,10 @@
 """
 import traceback
 
+from sqlalchemy.exc import PendingRollbackError
+
 import zeeguu.core
-from zeeguu.logging import log
+from zeeguu.logging import log, logp
 from zeeguu.core.content_retriever.article_downloader import download_from_feed
 from zeeguu.core.model import Feed, Language
 
@@ -29,6 +31,7 @@ db_session = zeeguu.core.model.db.session
 def download_for_feeds(list_of_feeds):
     counter = 0
     all_feeds_count = len(list_of_feeds)
+
     for feed in list_of_feeds:
         if feed.deactivated:
             continue
@@ -41,12 +44,18 @@ def download_for_feeds(list_of_feeds):
 
             download_from_feed(feed, zeeguu.core.model.db.session)
 
+        except PendingRollbackError as e:
+            db_session.rollback()
+            logp("Something went wrong and we had to rollback a transaction; following is the full stack trace:")
+            traceback.print_exc()
+
         except:
             traceback.print_exc()
 
+    print(f"Successfully finished processing {counter} feeds.")
+
 
 def retrieve_articles_for_language(language_code):
-
     language = Language.find(language_code)
     all_language_feeds = (
         Feed.query.filter_by(language_id=language.id)
@@ -58,7 +67,6 @@ def retrieve_articles_for_language(language_code):
 
 
 def retrieve_articles_from_all_feeds():
-
     counter = 0
     all_feeds = Feed.query.all()
     download_for_feeds(all_feeds)
