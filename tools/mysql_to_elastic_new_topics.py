e51fa70d0c0649a3253b6b0069ce8d3967e3caff
Tiago Ribeiro <tfnribeiro@gmail.com>
2024-04-17 14:08:36 +0200

    Renamed import to be more clear
    ~ Renamed the file to make it more explicit what it's doing.

--- tools/mysql_to_elastic_new_topics.py
+++ tools/mysql_to_elastic_new_topics.py
@@ -14,7 +14,7 @@ from datetime import datetime
 from sqlalchemy.orm.exc import NoResultFound
 from zeeguu.api.app import create_app
 
-from zeeguu.core.model import Topic
+from zeeguu.core.model import Topic, NewArticleTopicMap
 from zeeguu.core.model.article import article_topic_map
 from zeeguu.core.elastic.settings import ES_ZINDEX, ES_CONN_STRING
 import numpy as np
@@ -41,26 +41,13 @@ def find_topics(article_id, session):
     return topics.rstrip()
 
 
-def main(starting_index, delete_index=False):
+def main(delete_index=False):
     if delete_index:
         try:
             es.options(ignore_status=[400, 404]).indices.delete(index="zeeguu")
             print("Deleted index 'zeeguu'!")
         except Exception as e:
             print(f"Failed to delete: {e}")
-
-    def fetch_articles(max_id, min_id):
-        for i in range(max_id, min_id, -1):
-            print(f"Article '{i}'")
-            try:
-                article = Article.find_by_id(i)
-                if article:
-                    topics = find_topics(article.id, db_session)
-                    yield (article, topics)
-            except NoResultFound:
-                print(f"fail for: '{i}'")
-            except Exception as e:
-                print(f"fail for: '{i}', {e}")
 
     def fetch_articles_by_id(id_list):
         for i in id_list:
@@ -84,30 +71,29 @@ def main(starting_index, delete_index=False):
             except Exception as e:
                 print(f"fail for: '{article.id}', {e}")
 
-    # max_id = db_session.query(func.max(Article.id)).first()[0]
-    # min_id = db_session.query(func.min(Article.id)).first()[0]
-    sample_ids = np.array([a_id[0] for a_id in db_session.query(Article.id).all()])
+    # Sample Articles that have topics assigned and are not inferred
+    sample_ids = np.array(
+        [
+            a_id[0]
+            for a_id in db_session.query(Article.id)
+            .join(NewArticleTopicMap)
+            .filter(NewArticleTopicMap.origin_type != 3)
+            .all()
+        ]
+    )
+    sample_ids_no_in_es = list(
+        filter(lambda x: not es.exists(index=ES_ZINDEX, id=x), sample_ids)
+    )
+    print("Total articles missing: ", len(sample_ids_no_in_es))
     # I noticed that if a document is not added then it won't let me query the ES search.
     total_added = 0
-    total_iter = 100
+    total_iter = 400
     for i in range(total_iter):
-        sub_sample = np.random.choice(sample_ids, 10, replace=False)
-        print(f"starting import at: {sub_sample[0]}")
-        # print(f"max id in db: {sample_ids}")
-        # fetch_db_articles = list(gen_docs(fetch_articles(max_id, max_id - 200)))
-        # print(fetch_db_articles[:5])
-        # for success, info in parallel_bulk(es, fetch_db_articles):
-        #    if not success:
-        #        print("A document failed:", info)
-        # fetch_db_articles = fetch_articles(max_id, max_id - 200)
-        # for i in sub_sample:
-        #    res = create_or_update(Article.find_by_id(i), db_session)
-        #    print(res)
-        #    total_added += 1
+        print(f"Iteration {i+1}/{total_iter}...")
+        sub_sample = np.random.choice(sample_ids_no_in_es, 10, replace=False)
         res, _ = bulk(es, gen_docs(fetch_articles_by_id(sub_sample)))
         total_added += res
-        print(f"Iteration {i+1}/{total_iter}...")
-    print(total_added)
+    print("Total articles added: " + total_added)
 
 
 if __name__ == "__main__":
@@ -115,12 +101,7 @@ if __name__ == "__main__":
     print("waiting for the ES process to boot up")
     start = datetime.now()
     print(f"started at: {start}")
-    starting_index = 0
-
-    if len(sys.argv) > 1:
-        starting_index = int(sys.argv[1])
-
-    main(starting_index)
+    main()
     end = datetime.now()
     print(f"ended at: {end}")
     print(f"Process took: {end-start}")
