df76f74fe9e7188909c58be788ff492540ce7007
Mircea Filip Lungu <mircea.lungu@gmail.com>
2024-05-01 13:42:07 +0200

    git got confused

--- tools/migrations/24-05-update_learning_cycle_for_existing_users.sql
+++
@@ -1,6 +0,0 @@
-/* 
- Updates the learning cycle column to receptive for scheduled and learned bookmarks.
- */
-UPDATE bookmark b SET learning_cycle = 1
-WHERE b.id in (SELECT bookmark_id FROM basic_sr_schedule) -- bookmarks that are scheduled
-OR b.learned = 1; -- bookmarks that are learned
