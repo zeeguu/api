f8f456e1f2c07a8f0fe125903e47ace6f188300b
Klara Krag <klak@itu.dk>
2024-07-28 22:02:27 +0200

    setup of send_subscription_emails, and scheduler.

--- zeeguu/core/emailer/subscribed_search_email.py
+++ zeeguu/core/emailer/subscribed_search_email.py
@@ -1,5 +1,12 @@
+import datetime
+from zeeguu.api import app
+from zeeguu.core.content_recommender.elastic_recommender import article_search_for_user
 from zeeguu.core.model import User
 from zeeguu.core.emailer.zeeguu_mailer import ZeeguuMailer
+from zeeguu.core.model.search import Search
+from zeeguu.core.model.search_subscription import SearchSubscription
+from apscheduler.schedulers.background import BackgroundScheduler
+
 
 def send_mail_new_articles_search(to_email, search):
     body = "\r\n".join([
@@ -13,10 +20,28 @@ def send_mail_new_articles_search(to_email, search):
 
     emailer = ZeeguuMailer('New articles to your subscribed search', body, to_email)
     emailer.send()
-"""
+
 def send_subscription_emails():
-    # find all user, which is subscribed to searches and want to receive email.
-    # crawl for all users
-    #for each user, and the search they want an email to:
-    # send_mail_new_articles_search(to_email, search)
-"""
+    all_subscriptions = SearchSubscription.query.filter_by(receive_email=True).all()
+    all_searches = Search.query.all()
+    current_datetime = datetime.now(datetime.timezone.utc)
+    previous_day_datetime = current_datetime - datetime.timedelta(days=1)
+
+    for subscription in all_subscriptions:
+        for search in all_searches:
+            if subscription.search_id == search.id:
+                articles = article_search_for_user(subscription.user_id, 2, search.keywords, page = 0)
+                new_articles_found = [article for article in articles if article.published > previous_day_datetime]
+
+                if new_articles_found:
+                    user = User.find_by_id(subscription.user_id)
+                    send_mail_new_articles_search(user.email, search)
+
+def start_scheduler():
+    scheduler = BackgroundScheduler()
+    scheduler.add_job(send_subscription_emails, 'cron', hour=8, minute=0)
+    scheduler.start()
+
+if __name__ == "__main__":
+    start_scheduler()
+    app.run(debug=True)
