14b29e41625cdfed15b00dbb76c418a094876f55
Mircea Filip Lungu <mircea.lungu@gmail.com>
2024-07-31 10:00:55 +0200

    using the correct version of this file

--- tools/send_subscription_emails.py
+++ tools/send_subscription_emails.py
@@ -11,45 +11,58 @@ app.app_context().push()
 
 
 def send_mail_new_articles_search(to_email, search):
-    body = "\r\n".join([
-        "Hi there,",
-        " ",
-        "There are new articles related to your subscribed search: " + str(search) + ".",
-        " ",
-        "Cheers,",
-        "The Zeeguu Team"
-    ])
+    body = "\r\n".join(
+        [
+            "Hi there,",
+            " ",
+            "There are new articles related to your subscribed search: "
+            + str(search)
+            + ".",
+            " ",
+            "Cheers,",
+            "The Zeeguu Team",
+        ]
+    )
 
-    emailer = ZeeguuMailer('New articles to your subscribed search', body, to_email)
+    emailer = ZeeguuMailer("New articles to your subscribed search", body, to_email)
     emailer.send()
+
 
 def send_subscription_emails():
     all_subscriptions = SearchSubscription.query.filter_by(receive_email=True).all()
-    current_datetime = datetime.now(datetime.timezone.utc)
+    current_datetime = datetime.datetime.now()
     previous_day_datetime = current_datetime - datetime.timedelta(days=1)
 
     for subscription in all_subscriptions:
-        articles = article_search_for_user(subscription.user_id, 2, subscription.search, page = 0)
-        new_articles_found = [article for article in articles if article.published > previous_day_datetime]
+        user = User.find_by_id(subscription.user_id)
+        articles = article_search_for_user(
+            user, 2, subscription.search.keywords, page=0
+        )
+        new_articles_found = [
+            article
+            for article in articles
+            if article.published_time > previous_day_datetime
+        ]
 
         if new_articles_found:
-            user = User.find_by_id(subscription.user_id)
+
             send_mail_new_articles_search(user.email, subscription.search)
-            print(f"""
+            print(
+                f"""
                 ####################################
                   email send to {user.email} for: {subscription.search}
                 ###################################
-        """)
+        """
+            )
         else:
-            print(f"""
+            print(
+                f"""
                 ####################################
-                  No new articles found {subscription.search}
+                  No new articles found {subscription.search} for user: {user.email}
                 ###################################
-        """)
+        """
+            )
+
 
 if __name__ == "__main__":
     send_subscription_emails()
-
-    #python article_crawler.py da
-    #_plaground
-    #move to tools...
