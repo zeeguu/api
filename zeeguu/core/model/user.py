c0a64cd74451f402b1034b8ee0e84b28dcaa5b27
Tiago Ribeiro <tfnribeiro@gmail.com>
2024-09-09 17:53:15 +0200

    Initialize before_date inside method (#200)
    - Seems the issue was due to the fact that the before date was initialized in the method's definition and would be cached. After a day of running, the API would only return the bookmarks before the day it was initialized.

--- zeeguu/core/model/user.py
+++ zeeguu/core/model/user.py
@@ -198,7 +198,6 @@ class User(db.Model):
 
         if session:
             session.add(language)
-            
 
     def set_learned_language_level(
         self, language_code: str, cefr_level: str, session=None
@@ -397,11 +396,13 @@ class User(db.Model):
     def all_bookmarks(
         self,
         after_date=datetime.datetime(1970, 1, 1),
-        before_date=datetime.date.today() + datetime.timedelta(days=1),
+        before_date=None,
         language_id=None,
     ):
         from zeeguu.core.model import Bookmark, UserWord
 
+        if before_date is None:
+            before_date = datetime.date.today() + datetime.timedelta(days=1)
         query = zeeguu.core.model.db.session.query(Bookmark)
 
         query = query.join(UserWord, Bookmark.origin_id == UserWord.id)
