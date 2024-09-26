caed4905d1e4203eb5b6d0e1d33c20761cd96e78
Tiago Ribeiro <tfnribeiro@gmail.com>
2024-09-26 15:46:07 +0200

    Removing VISIBLE, to avoid problems in Prod

--- tools/migrations/24-09-24--add_feeback_tables.sql
+++ tools/migrations/24-09-24--add_feeback_tables.sql
@@ -47,9 +47,9 @@ CREATE TABLE `zeeguu_test`.`user_feedback_report` (
     `report_time` DATETIME NULL,
     `url_id` INT NULL,
     PRIMARY KEY (`id`),
-    INDEX `user_feedback_report_ibfk_1_idx` (`user_id` ASC) VISIBLE,
-    INDEX `user_feedback_report_ibfk_2_idx` (`feedback_component_id` ASC) VISIBLE,
-    INDEX `user_feedback_report_ibfk_2_idx1` (`url_id` ASC) VISIBLE,
+    INDEX `user_feedback_report_ibfk_1_idx` (`user_id` ASC),
+    INDEX `user_feedback_report_ibfk_2_idx` (`feedback_component_id` ASC),
+    INDEX `user_feedback_report_ibfk_2_idx1` (`url_id` ASC),
     CONSTRAINT `user_feedback_report_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `zeeguu_test`.`user` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
     CONSTRAINT `user_feedback_report_ibfk_2` FOREIGN KEY (`feedback_component_id`) REFERENCES `zeeguu_test`.`feedback_component` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
     CONSTRAINT `user_feedback_report_ibfk_3` FOREIGN KEY (`url_id`) REFERENCES `zeeguu_test`.`url` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
