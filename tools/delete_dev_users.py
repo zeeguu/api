931f4999a3a2838261a66bab3422ad17f0caee05
Mircea Filip Lungu <mircea.lungu@gmail.com>
2024-09-27 20:03:00 +0300

    Update delete_dev_users.py

--- tools/delete_dev_users.py
+++ tools/delete_dev_users.py
@@ -9,12 +9,17 @@ app.app_context().push()
 db_session = zeeguu.core.model.db.session
 
 # delete all anonymous users
+anon_users = 0
 for user in User.find_all():
     if "anon.zeeguu" in user.email:
         delete_user_account(db_session, user)
+        anon_users += 1
 
+dev_users = 0
 for user in User.query.filter_by(is_dev=True):
     print("deleting ... " + user.name)
     delete_user_account(db_session, user)
+    dev_users += 1
 
+print("Deleted: anon=" + str(anon_users) + ", dev=" + str(dev_users))
 print("Remaining users: " + str(len(User.find_all())))
