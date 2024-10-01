7a5dadd2ccc1151d703d0f9d658fcfe8093d6d0b
Tiago Ribeiro <tfnribeiro@gmail.com>
2024-10-01 10:41:29 +0200

    Adding general invitation code

--- zeeguu/api/endpoints/feature_toggles.py
+++ zeeguu/api/endpoints/feature_toggles.py
@@ -58,6 +58,7 @@ def _merle_exercises(user):
         user.invitation_code == "Merle"
         or user.invitation_code == "MerleITU"
         or user.invitation_code == "PTCT"
+        or user.invitation_code == "zeeguu-preview"
         or user.id in [534, 2953, 4022, 4089, 4607]
     )
     return right_user
