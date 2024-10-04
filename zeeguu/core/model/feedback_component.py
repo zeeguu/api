4359af7d571a46a927839893e658ff63b4605e62
Tiago Ribeiro <tfnribeiro@gmail.com>
2024-10-04 23:31:46 +0200

    Adding Feedback Table to support User Feedback (#229)
    * Added support for Feedback in the DB model: two new tables to allow support for User feedback from a popup in the app.

---
+++ zeeguu/core/model/feedback_component.py
@@ -0,0 +1,67 @@
+import sqlalchemy
+from zeeguu.api.utils.abort_handling import make_error
+import zeeguu.core
+from sqlalchemy import Column, Integer, String
+
+from zeeguu.core.model import db
+
+
+class FeedbackComponent(db.Model):
+    """
+
+    A Feedback Component is the UI component that designates the context in which a user reports feedback.
+    They pick one of the Feedback Components from a drop-down list. The available
+    components can be adjusted in the frontend, e.g. the exercises won't have the
+    components related to the Article Recommender or Reader.
+
+    """
+
+    __table_args__ = {"mysql_collate": "utf8_bin"}
+
+    id = Column(Integer, primary_key=True)
+    component_type = Column(String(45))
+
+    def __init__(self, component_type):
+        self.component_type = component_type
+
+    def __repr__(self):
+        return f"<FeedbackComponent: {self.component_type}>"
+
+    def as_dictionary(self):
+
+        return dict(
+            id=self.id,
+            component_type=self.component_type,
+        )
+
+    @classmethod
+    def get_all_options(cls):
+        return cls.query.all()
+
+    @classmethod
+    def find_or_create(cls, session, component_type):
+        try:
+            return cls.query.filter(cls.component_type == component_type).one()
+        except sqlalchemy.orm.exc.NoResultFound:
+            new = cls(component_type)
+            session.add(new)
+            return new
+
+    @classmethod
+    def find(cls, component_type):
+        try:
+            search = cls.query.filter(cls.component_type == component_type).one()
+            return search
+        except sqlalchemy.orm.exc.NoResultFound:
+            return None
+
+    @classmethod
+    def find_by_id(cls, i):
+        try:
+            result = cls.query.filter(cls.id == i).one()
+            return result
+        except Exception as e:
+            from sentry_sdk import capture_exception
+
+            capture_exception(e)
+            return None
---
+++ zeeguu/core/model/user_feedback.py
@@ -0,0 +1,74 @@
+from sqlalchemy import String, DateTime
+from sqlalchemy.orm import relationship
+from zeeguu.core.model.user import User
+from zeeguu.core.model.url import Url
+from zeeguu.core.model.feedback_component import FeedbackComponent
+from zeeguu.core.util.time import get_server_time_utc
+
+import sqlalchemy
+
+import zeeguu.core
+
+from zeeguu.core.model import db
+
+
+class UserFeedback(db.Model):
+    """
+    This contains the message the user sends with the component they pick.
+    We also store the report time in UTC and the URL where the user was
+    when they send the message.
+
+    """
+
+    __table_args__ = {"mysql_collate": "utf8_bin"}
+    __tablename__ = "user_feedback"
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey(User.id))
+    user = relationship(User)
+
+    feedback_component_id = db.Column(db.Integer, db.ForeignKey(FeedbackComponent.id))
+    feedback_component = relationship(FeedbackComponent)
+
+    message = db.Column(String(512))
+    report_time = db.Column(DateTime)
+
+    url_id = db.Column(db.Integer, db.ForeignKey(Url.id))
+    url = relationship(Url)
+
+    def __init__(
+        self, user: User, feedback_component: FeedbackComponent, message: str, url: Url
+    ):
+        self.user = user
+        self.feedback_component = feedback_component
+        self.message = message
+        self.report_time = get_server_time_utc()
+        self.url = url
+
+    def __str__(self):
+        return f"User Feedback: ('{self.message}', {self.feedback_component.component_type})"
+
+    __repr__ = __str__
+
+    @classmethod
+    def create(
+        cls,
+        session,
+        user: User,
+        feedback_component: FeedbackComponent,
+        message: str,
+        url: Url,
+    ):
+        new = cls(user, feedback_component, message, url)
+        session.add(new)
+        return new
+
+    @classmethod
+    def all_for_user(cls, user):
+        return cls.query.filter(cls.user == user).all()
+
+    @classmethod
+    def with_feedback_component_id(cls, feedback_component_id):
+        return (
+            cls.query.filter(cls.feedback_component_id == feedback_component_id)
+        ).all()
