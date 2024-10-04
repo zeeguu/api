1c2cdbb740dd8667326845eaed2b620ba3203b5f
Mircea Filip Lungu <mircea.lungu@gmail.com>
2024-10-04 21:18:36 +0200

    tiniest rename

--- zeeguu/core/word_filter/profanity_filter.py
+++ zeeguu/core/word_filter/profanity_filter.py
@@ -3,13 +3,13 @@ import os
 
 SKIP_FILES = set(["README.md", "USERS.md", "LICENSE"])
 MODULE_PATH = Path(__file__).parent.absolute()
-PATH_TO_WORD_LIST = os.path.join(MODULE_PATH, "data", "bad-words")
+BAD_WORDS_FOLDER = os.path.join(MODULE_PATH, "data", "bad-words")
 
 
 def load_bad_words():
     bad_word_list = set()
-    for f in os.listdir(PATH_TO_WORD_LIST):
-        path_to_file = os.path.join(PATH_TO_WORD_LIST, f)
+    for f in os.listdir(BAD_WORDS_FOLDER):
+        path_to_file = os.path.join(BAD_WORDS_FOLDER, f)
         if os.path.isfile(path_to_file) and f not in SKIP_FILES:
             with open(path_to_file, "r", encoding="utf-8") as f:
                 lines = f.readlines()
