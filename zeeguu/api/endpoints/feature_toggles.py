128250ebfc6835c430c4c7ade3016d1e82b01d4f
Tiago Ribeiro <tfnribeiro@gmail.com>
2024-10-07 13:34:43 +0200

    Disabled feature for all users
    - Instead, use only for users with the preview code.

--- zeeguu/api/endpoints/feature_toggles.py
+++ zeeguu/api/endpoints/feature_toggles.py
@@ -55,8 +55,13 @@ def _feature_map():
 
 
 def _new_topics(user):
-    right_user = user.id == 534 or user.id == 4022 or user.id == 4089
-    return right_user or True
+    right_user = (
+        user.id == 534
+        or user.id == 4022
+        or user.id == 4089
+        or user.invitation_code == "zeeguu-preview"
+    )
+    return right_user
 
 
 def _tiago_exercises(user):
