ba2ee060293a973ccd38b30694f474480458d2ce
Tiago Ribeiro <tfnribeiro@gmail.com>
2024-10-08 15:28:01 +0200

    Add specificity to query to make it faster
    - Limit the query to only articles which have keywords and haven't been mapped yet.

--- tools/es_v8_migration/set_new_topics_from_url_keyword.py
+++ tools/es_v8_migration/set_new_topics_from_url_keyword.py
@@ -7,7 +7,12 @@
 
 import zeeguu.core
 from zeeguu.api.app import create_app
-from zeeguu.core.model import Article
+from zeeguu.core.model import (
+    Article,
+    ArticleUrlKeywordMap,
+    UrlKeyword,
+    NewArticleTopicMap,
+)
 from tqdm import tqdm
 
 app = create_app()
@@ -18,10 +23,21 @@ db_session = zeeguu.core.model.db.session
 counter = 0
 
 # languages = Language.available_languages()
-print("Adding topics keywords to articles!")
-all_article_id = [a_id[0] for a_id in db_session.query(Article.id).all()]
-total_articles = len(all_article_id)
-for a_id in tqdm(all_article_id):
+print("Finding all articles with Url Keywords with Topics assigned...")
+print("(Articles which already have a NewTopic mapping are ignored.)")
+a_url_keywords_w_new_topics = (
+    db_session.query(Article.id)
+    .join(ArticleUrlKeywordMap)
+    .join(UrlKeyword)
+    .join(NewArticleTopicMap, isouter=True)
+    .filter(UrlKeyword.new_topic != None)
+    .filter(NewArticleTopicMap.new_topic_id == None)
+    .all()
+)
+print("Adding topics based on url keywords to articles...")
+total_articles = len(a_url_keywords_w_new_topics)
+for article in tqdm(a_url_keywords_w_new_topics):
+    a_id = article[0]
     counter += 1
     try:
         article = Article.find_by_id(a_id)
@@ -36,7 +52,6 @@ for a_id in tqdm(all_article_id):
             topics_added.add(topic.id)
             topics.append(topic)
         article.set_new_topics(topics, db_session)
-        db_session.add(article)
     except Exception as e:
         counter -= 1
         print(f"Failed for article id: {a_id}, with: {e}")
