8933dd78350cab425edc4641645694a6ab2f9aa6
Mircea Filip Lungu <mircea.lungu@gmail.com>
2024-10-17 15:47:30 +0200

    bugfix: for cohorts with no language

--- zeeguu/core/user_statistics/exercise_sessions.py
+++ zeeguu/core/user_statistics/exercise_sessions.py
@@ -9,6 +9,13 @@ from zeeguu.core.model.cohort import Cohort
 def total_time_in_exercise_sessions(user_id, cohort_id, start_time, end_time):
     # TODO: use also the cohort_id somehow
     cohort = Cohort.find(cohort_id)
+
+    same_language_as_cohort_condition = ""
+    if cohort.language_id:
+        same_language_as_cohort_condition = (
+            f" WHERE uw.language_id = {cohort.language_id} "
+        )
+
     query = f"""
         select sum(duration)
         from user_exercise_session as ues
@@ -16,7 +23,7 @@ def total_time_in_exercise_sessions(user_id, cohort_id, start_time, end_time):
                         INNER JOIN bookmark_exercise_mapping bem on e.id = bem.exercise_id
                         INNER JOIN bookmark b ON bem.bookmark_id = b.id
                         INNER JOIN user_word uw ON b.origin_id = uw.id
-                        WHERE uw.language_id = {cohort.language_id})
+                        {same_language_as_cohort_condition})
         and ues.start_time > :start_time
         and ues.last_action_time < :end_time
         and ues.user_id = :user_id
