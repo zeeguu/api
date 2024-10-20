844bd6bdec7c678c694f30d9e9c92816c376de41
Mircea Filip Lungu <mircea.lungu@gmail.com>
2024-10-20 13:01:52 +0300

    added download_feed tool

---
+++ tools/download_feed.py
@@ -0,0 +1,12 @@
+from tools.crawl_summary.crawl_report import CrawlReport
+from tools.feed_retrieval import download_for_feeds
+from zeeguu.core.model import Feed
+from zeeguu.api.app import create_app
+
+app = create_app()
+app.app_context().push()
+
+crawl_report = CrawlReport()
+
+feed = Feed.find_by_id(85)
+download_for_feeds([feed], crawl_report)
