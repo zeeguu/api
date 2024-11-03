d7b3c25de4286adff864f8054056aa80146758fa
Mircea Filip Lungu <mircea.lungu@gmail.com>
2024-11-03 21:01:16 +0100

    nicer printing of students in classes

--- tools/users_recently_active.py
+++ tools/users_recently_active.py
@@ -13,7 +13,7 @@ app.app_context().push()
 
 from collections import defaultdict
 
-teacher_student_map = defaultdict(list)
+cohort_student_map = defaultdict(list)
 
 from zeeguu.core.model import User
 
@@ -22,11 +22,18 @@ for user_id in User.all_recent_user_ids():
     # print(f"{user.name} ({user.email})")
     for ucmap in user.cohorts:
         # print(f"{ucmap.cohort.name}")
-        for teacher in ucmap.cohort.get_teachers():
-            # print(f"{teacher.name}")
-            teacher_student_map[teacher].append(user.name)
+        # print(f"{teacher.name}")
+        cohort_student_map[ucmap.cohort].append(user.name)
 
     # print("")
-for key, values in teacher_student_map.items():
+for cohort, values in cohort_student_map.items():
+    print(f"============================")
+    print(f"{cohort.name} ({cohort.language.code if cohort.language else ''})")
+    print(f"============================")
+
+    for teacher in cohort.get_teachers():
+        print(f"  {teacher.name} ({teacher.email})")
     for v in values:
-        print(key.email, " : ", v)
+        print("  - ", v)
+
+    print(" ")
