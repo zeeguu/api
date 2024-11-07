f90c9754ed08149e0ea9695385f627ca17e63903
Tiago Ribeiro <tfnribeiro@gmail.com>
2024-11-07 10:38:56 +0100

    Added a way to skip testing the feed
    - Feeds can be tested locally and in production we can skip this test, especially for newspaper it takes quite a while to crawl.

--- tools/add_feed.py
+++ tools/add_feed.py
@@ -8,9 +8,10 @@ app = create_app()
 app.app_context().push()
 
 
-def create_and_test_feed(url: str, feed_type: int):
+def create_and_test_feed(url: str, feed_type: int, test_feed: bool):
     feed = Feed.from_url(url, feed_type=feed_type)
-    print(feed.feed_health_info())
+    if test_feed:
+        print(feed.feed_health_info())
 
     return feed
 
@@ -19,17 +20,23 @@ def main():
     _feed_url = input("Feed url:  ")
     print(f"Available feed types: '{FEED_TYPE}'")
     feed_type = int(input("What feed type is it? : "))
-    test_feed = create_and_test_feed(_feed_url, feed_type)
+    test_feed = input("Do you want to test to parse the feed (1=yes, else n) : ") == "1"
+    feed = create_and_test_feed(_feed_url, feed_type, test_feed)
 
-    feed_name = input(f"Feed name (Enter for: {test_feed.title}):  ") or test_feed.title
+    feed_name = input(f"Feed name (Enter for: {feed.title}):  ") or feed.title
     print(f"= {feed_name}")
 
-    icon_name = input("Icon name to be found in resources folder (e.g. 20min.png):  ")
+    default_icon_name = f"{feed_name.lower().replace(" ", "-")}.png"
+    icon_name = (
+        input(
+            f"Icon name to be found in resources folder (e.g. {default_icon_name}):  "
+        )
+        or default_icon_name
+    )
     print(f"= {icon_name}")
 
     description = (
-        input(f"Description (Enter for: {test_feed.description}): ")
-        or test_feed.description
+        input(f"Description (Enter for: {feed.description}): ") or feed.description
     )
     print(f"= {description}")
 
