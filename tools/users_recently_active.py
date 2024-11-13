21856ebc531f41f48b2007e17befd638c6a2528e
Mircea Filip Lungu <mircea.lungu@gmail.com>
2024-11-13 12:15:54 +0100

    Update users_recently_active.py

--- tools/users_recently_active.py
+++ tools/users_recently_active.py
@@ -6,6 +6,10 @@
 
 """
 
+DAYS_SINCE_ACTIVE = 30
+SHOW_TEACHER_NAMES = False
+SHOW_STUDENT_NAMES = False
+
 from zeeguu.api.app import create_app
 
 app = create_app()
@@ -17,7 +21,7 @@ cohort_student_map = defaultdict(list)
 
 from zeeguu.core.model import User
 
-for user_id in User.all_recent_user_ids():
+for user_id in User.all_recent_user_ids(DAYS_SINCE_ACTIVE):
     user = User.find_by_id(user_id)
     # print(f"{user.name} ({user.email})")
     for ucmap in user.cohorts:
@@ -26,14 +30,29 @@ for user_id in User.all_recent_user_ids():
         cohort_student_map[ucmap.cohort].append(user.name)
 
     # print("")
-for cohort, values in cohort_student_map.items():
-    print(f"============================")
-    print(f"{cohort.name} ({cohort.language.code if cohort.language else ''})")
-    print(f"============================")
+
+ordered_cohorts = sorted(
+    cohort_student_map.keys(), key=lambda x: len(cohort_student_map[x]), reverse=True
+)
+
+print(f"Users active in the last {DAYS_SINCE_ACTIVE} days")
+for cohort in ordered_cohorts:
+    values = cohort_student_map[cohort]
+    print(f"========================================================")
+    print(
+        f"{cohort.name} ({cohort.id}) "
+        f"\nLang: {cohort.language.code if cohort.language else ''} "
+        f"\nInv Code: {cohort.inv_code} "
+        f"\nActive Students: {len(values)}"
+    )
+    print(f"========================================================")
 
     for teacher in cohort.get_teachers():
-        print(f"  {teacher.name} ({teacher.email})")
+        if SHOW_TEACHER_NAMES:
+            print(f"  {teacher.name} ({teacher.email})")
+
     for v in values:
-        print("  - ", v)
+        if SHOW_STUDENT_NAMES:
+            print("  - ", v)
 
     print(" ")
