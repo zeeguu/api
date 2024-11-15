c31f587f00d051eb516c9ba21f281f23bddc00a1
Tiago Ribeiro <tfnribeiro@gmail.com>
2024-11-15 10:18:44 +0100

    Added test for adding article topics

--- zeeguu/core/test/test_article.py
+++ zeeguu/core/test/test_article.py
@@ -3,9 +3,11 @@ from unittest import TestCase
 from zeeguu.core.test.model_test_mixin import ModelTestMixIn
 
 import zeeguu.core
+from zeeguu.core.model.article_topic_map import TopicOriginType
 from zeeguu.core.test.rules.article_rule import ArticleRule
 from zeeguu.core.test.rules.language_rule import LanguageRule
-from zeeguu.core.model import Article
+from zeeguu.core.test.rules.topic_rule import TopicRule
+from zeeguu.core.model import Article, Topic
 from zeeguu.core.test.mocking_the_web import (
     URL_CNN_KATHMANDU,
     URL_SPIEGEL_VENEZUELA,
@@ -20,6 +22,16 @@ class ArticleTest(ModelTestMixIn, TestCase):
         self.article1 = ArticleRule().article
         self.article2 = ArticleRule().article
         self.language = LanguageRule.get_or_create_language("en")
+
+    def test_add_topic(self):
+        sports = TopicRule.get_or_create_topic(1)
+        health_society = TopicRule.get_or_create_topic(5)
+        self.article1.add_topic(health_society, session, TopicOriginType.HARDSET)
+        self.article1.add_topic(sports, session, TopicOriginType.HARDSET)
+        assert len(self.article1.topics) == 2
+        article_topics = [atm.topic for atm in self.article1.topics]
+        assert sports in article_topics
+        assert health_society in article_topics
 
     def test_articles_are_different(self):
         assert self.article1.title != self.article2.title
