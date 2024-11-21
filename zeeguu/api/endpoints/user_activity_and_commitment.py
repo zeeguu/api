029199a22ec70ea0e90aabdd5fbe6680beeaa456
jolb <jolb@itu.dk>
2024-11-21 09:26:11 +0100

    added comment in user_activity_and_commitment file

--- zeeguu/api/endpoints/user_activity_and_commitment.py
+++ zeeguu/api/endpoints/user_activity_and_commitment.py
@@ -1,4 +1,5 @@
 #not sure yet what all this is. Have a look at it.
+#To do: Import the file where the activity_and_commitment_by_user is located
 
 import flask
 
@@ -8,13 +9,16 @@ from . import api
 from zeeguu.api.utils.route_wrappers import cross_domain, requires_session
 from zeeguu.core.model import User
 
-@api.route("/data_user_commitment", method=("GET",))
+@api.route("/user_activity_and_commitment", method=("GET",))
 @cross_domain
 @requires_session
 
-def data_user_commitment():
+def user_activity_and_commitment():
 	"""
 	User commitment info 
 	"""
 	user= User.find_by_id(flask.g.user_id)
-	return json_result(commitment_by_user(user))
+	return json_result(activity_and_commitment_by_user(user))
+	
+	
+
