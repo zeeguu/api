c736045382f47e4fe91546788f2da460add2ac19
Mircea Filip Lungu <mircea.lungu@gmail.com>
2024-11-30 14:07:25 +0100

    distinguishing between active and inactive

--- tools/feeds_for_language.py
+++ tools/feeds_for_language.py
@@ -8,7 +8,16 @@ app.app_context().push()
 language = Language.find(input("Language code: "))
 all_italian_feeds = Feed.query.filter_by(language_id=language.id).all()
 
+active_feeds = [feed for feed in all_italian_feeds if not feed.deactivated]
+inactive_feeds = [feed for feed in all_italian_feeds if feed.deactivated]
 
 print(f"Feeds for {language.name}: ")
-for feed in all_italian_feeds:
+for feed in active_feeds:
+    deactivated_status = "Deactivated" if feed.deactivated else ""
     print(f"  - {feed.title} ({feed.url.domain_name()})")
+
+
+if inactive_feeds:
+    print(f"Inactive feeds for {language.name}: ")
+    for feed in inactive_feeds:
+        print(f"  - {feed.title} ({feed.url.domain_name()})")
