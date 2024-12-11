e8a96d816a70ca8f9be6167188ac20172c747d8f
Mircea Filip Lungu <mircea.lungu@gmail.com>
2024-12-11 17:08:06 +0100

    addressed tiago's observations

--- tools/feeds_for_language.py
+++ tools/feeds_for_language.py
@@ -1,23 +1,31 @@
 from zeeguu.api.app import create_app
+from zeeguu.core.feed_handler import FEED_TYPE, FEED_TYPE_TO_FEED_HANDLER
 
 from zeeguu.core.model import Language, Feed
 
 app = create_app()
 app.app_context().push()
 
-language = Language.find(input("Language code: "))
-all_italian_feeds = Feed.query.filter_by(language_id=language.id).all()
 
-active_feeds = [feed for feed in all_italian_feeds if not feed.deactivated]
-inactive_feeds = [feed for feed in all_italian_feeds if feed.deactivated]
+def print_feed(feed):
+    feed_names = dict((v, k) for k, v in FEED_TYPE.items())
+
+    feed_type = feed_names.get(feed.feed_type)
+    print(f"  - ({feed_type}): {feed.title} ({feed.url.as_string()})")
+
+
+language = Language.find(input("Language code: "))
+all_feeds_for_language = Feed.query.filter_by(language_id=language.id).all()
+
+active_feeds = [feed for feed in all_feeds_for_language if not feed.deactivated]
+inactive_feeds = [feed for feed in all_feeds_for_language if feed.deactivated]
 
 print(f"Feeds for {language.name}: ")
 for feed in active_feeds:
     deactivated_status = "Deactivated" if feed.deactivated else ""
-    print(f"  - {feed.title} ({feed.url.domain_name()})")
-
+    print_feed(feed)
 
 if inactive_feeds:
     print(f"Inactive feeds for {language.name}: ")
     for feed in inactive_feeds:
-        print(f"  - {feed.title} ({feed.url.domain_name()})")
+        print_feed(feed)
