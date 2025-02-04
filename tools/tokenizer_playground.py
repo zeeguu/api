68d11fe417930085265a221df8fab83de1a4a5f8
Tiago Ribeiro <tfnribeiro@gmail.com>
2025-02-04 15:26:55 +0100

    Update tokenizer_playground.py
    - Updated to new tokenizer classes.

--- tools/tokenizer_playground.py
+++ tools/tokenizer_playground.py
@@ -1,10 +1,11 @@
 from zeeguu.core.model.language import Language
-from zeeguu.core.tokenization.tokenizer import ZeeguuTokenizer, TokenizerModel
+from zeeguu.core.tokenization import TokenizerModel, get_tokenizer
 from zeeguu.api.app import create_app
 
 
 from pprint import pprint
 import os
+
 import stanza
 import time
 import spacy
@@ -16,15 +17,20 @@ def clear_terminal():
 
 app = create_app()
 app.app_context().push()
-en_spacy = spacy.load("en_core_web_sm")
+
+
+# en_spacy = spacy.load("en_core_web_sm")
+def print_token_list(token_list):
+    print([t["text"] for t in token_list])
+
 
 while True:
     clear_terminal()
     print("Welcome to the Zeeguu Tokenizer")
     language = Language.find(input("Enter language code (ex. 'en'): ").lower())
-    tokenizer_nltk = ZeeguuTokenizer(language, TokenizerModel.NLTK)
-    tokenizer_stanza = ZeeguuTokenizer(language, TokenizerModel.STANZA_TOKEN_ONLY)
-    tokenizer_stanza_pos = ZeeguuTokenizer(language, TokenizerModel.STANZA_TOKEN_POS)
+    tokenizer_nltk = get_tokenizer(language, TokenizerModel.NLTK)
+    tokenizer_stanza = get_tokenizer(language, TokenizerModel.STANZA_TOKEN_ONLY)
+    tokenizer_stanza_pos = get_tokenizer(language, TokenizerModel.STANZA_TOKEN_POS)
     text = input("Enter text: ")
     print("Got the following text: ", text)
     nltk_start = time.time()
@@ -38,20 +44,23 @@ while True:
     stanza_pos_tokens = tokenizer_stanza_pos.tokenize_text(text)
     stanza_pos_end = time.time() - stanza_pos_start
 
-    spacy_start = time.time()
-    spacy_tokens = en_spacy(text)
-    spacy_end = time.time() - spacy_start
+    # spacy_start = time.time()
+    # spacy_tokens = en_spacy(text)
+    # spacy_end = time.time() - spacy_start
     print("Total chars: ", len(text))
     print(f"Processing NLTK time: {nltk_end:.2f} seconds, for {len(tokens)} tokens")
+    print_token_list(tokens)
     print(
         f"Processing stanza time: {stanza_end:.2f} seconds, for {len(stanza_tokens)} tokens"
     )
     print(
         f"Processing stanza with POS time: {stanza_pos_end:.2f} seconds, for {len(stanza_pos_tokens)} tokens"
     )
-    print(
-        f"Processing spaCy time: {spacy_end:.2f} seconds, for {len(spacy_tokens)} tokens"
-    )
+    print_token_list(stanza_tokens)
+
+    # print(
+    #    f"Processing spaCy time: {spacy_end:.2f} seconds, for {len(spacy_tokens)} tokens"
+    # )
     if input("Enter 'n' to skip token details: ") != "n":
         print()
         print("#" * 10 + " NLTK " + "#" * 10)
@@ -63,8 +72,8 @@ while True:
             pprint(token)
             print("#" * 5)
         print("#" * 10 + " spaCy " + "#" * 10)
-        for t in spacy_tokens:
-            print(t, t.lemma_, t.pos_, t.dep_)
-            print("####")
+        # for t in spacy_tokens:
+        #     print(t, t.lemma_, t.pos_, t.dep_)
+        #     print("####")
     if input("Enter 'q' to quit, any other key to continue: ") == "q":
         break
