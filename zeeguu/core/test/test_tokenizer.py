4583a35a3059992532a04fcb7dcdcc43ddf8747c
Tiago Ribeiro <tfnribeiro@gmail.com>
2025-02-12 13:26:03 +0100

    Update test_tokenizer.py
    - Added a test to catch a possible error in tokenization.

--- zeeguu/core/test/test_tokenizer.py
+++ zeeguu/core/test/test_tokenizer.py
@@ -286,6 +286,23 @@ class TokenizationTest(ModelTestMixIn):
         assert tokens[1].is_like_num
         TokenizationTest.assert_sentence_i_token_i_is_correct(tokens)
 
+    def test_danish_tokenization_3(self):
+        # Found an error where "summer'i" was getting tokenized together.
+        # Ensure the new changes prevent this from happening.
+        text = """Sommeren 2024 fik tilnavnet 'cruel summer' i modebranchen."""
+        tokens = self.da_tokenizer.tokenize_text(text, False)
+        assert [
+            "Sommeren",
+            "2024",
+            "fik",
+            "tilnavnet",
+            "'cruel",
+            "summer'",
+            "i",
+            "modebranchen",
+            ".",
+        ] == [t.text for t in tokens]
+
     def test_portuguese_tokenization_1(self):
         text = """Eu estou a testar o Tokenizer nas frazes do Zeeguu."""
         tokens = self.pt_tokenizer.tokenize_text(text, False)
