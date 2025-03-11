514196c1b932ff45b79af0da1f3345843d42bef8
Tiago Ribeiro <tfnribeiro@gmail.com>
2025-03-11 09:08:13 +0100

    Updated formula
    - We noted that especially in smaller articles our estimation based on the scrolling percentage can perform quite poorly (not showing small articles as read). Instead of using the scroll percentage, we now use the total duration the user has read the article.

--- zeeguu/core/util/__init__.py
+++ zeeguu/core/util/__init__.py
@@ -6,3 +6,4 @@ from zeeguu.core.util.hash import text_hash, password_hash
 from zeeguu.core.util.time import get_server_time_utc
 from zeeguu.core.util.list import remove_duplicates_keeping_order
 from zeeguu.core.util.reading_time_estimator import estimate_read_time
+from .time_conversion import ms_to_m
--- zeeguu/core/util/reading_time_estimator.py
+++ zeeguu/core/util/reading_time_estimator.py
@@ -1,9 +1,9 @@
 import math
 
 
-def estimate_read_time(word_count: int):
+def estimate_read_time(word_count: int, ceil: bool = True):
     """
     Returns the estimated reading time in minutes, assuming
     a reading rate of 160WPM.
     """
-    return math.ceil(word_count / 160)
+    return math.ceil(word_count / 160) if ceil else word_count / 160
