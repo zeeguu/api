af54a06c0424beb6a8c8a2a6eef01bc646e4d59b
Mircea Filip Lungu <mircea.lungu@gmail.com>
2025-03-18 12:06:03 +0100

    small refactorings

--- zeeguu/core/util/compute_fk_word_count.py
+++ zeeguu/core/util/compute_fk_word_count.py
@@ -1,15 +1,16 @@
 from zeeguu.core.model.language import Language
+from zeeguu.core.language.difficulty_estimator_factory import (
+    DifficultyEstimatorFactory,
+)
+from zeeguu.core.tokenization import TOKENIZER_MODEL, get_tokenizer
 
 
 def compute_fk_and_wordcount(content, language: Language):
-    from zeeguu.core.language.difficulty_estimator_factory import (
-        DifficultyEstimatorFactory,
-    )
-    from zeeguu.core.tokenization import TOKENIZER_MODEL, get_tokenizer
 
     fk_estimator = DifficultyEstimatorFactory.get_difficulty_estimator("fk")
     fk_difficulty = fk_estimator.estimate_difficulty(content, language, None)
     tokenizer = get_tokenizer(language, TOKENIZER_MODEL)
+
     # easier to store integer in the DB
     # otherwise we have to use Decimal, and it's not supported on all dbs
     fk_difficulty = fk_difficulty["grade"]
