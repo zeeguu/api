7c2b5eae2150d2fd07e60b92a429d405b0ba4142
Tiago Ribeiro <tfnribeiro@gmail.com>
2025-04-11 08:21:38 +0200

    Updating only the article id

--- tools/update_elastic_ids.py
+++ tools/update_elastic_ids.py
@@ -1,6 +1,6 @@
 # coding=utf-8
 from zeeguu.core.elastic.indexing import (
-    create_or_update_doc_for_bulk,
+    update_article_ids_in_es_for_bulk,
 )
 from elasticsearch import Elasticsearch
 from elasticsearch.helpers import bulk, scan
@@ -15,7 +15,7 @@ from tqdm import tqdm
 app = create_app()
 app.app_context().push()
 
-ITERATION_STEP = 100
+ITERATION_STEP = 1000
 
 print(ES_CONN_STRING)
 es = Elasticsearch(ES_CONN_STRING)
@@ -41,7 +41,7 @@ def main():
     def gen_docs(articles_w_topics):
         for article in articles_w_topics:
             try:
-                yield create_or_update_doc_for_bulk(article, db_session)
+                yield update_article_ids_in_es_for_bulk(article)
             except Exception as e:
                 print(f"fail for: '{article.id}', {e}")
 
