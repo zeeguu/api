c3996ecd47b401df6d8af11dcd738e1b2cc3de84
Mircea Filip Lungu <mircea.lungu@gmail.com>
2025-05-10 21:57:55 +0200

    formatting

--- zeeguu/api/endpoints/user_preferences.py
+++ zeeguu/api/endpoints/user_preferences.py
@@ -7,6 +7,8 @@ from zeeguu.api.utils.route_wrappers import cross_domain, requires_session
 from . import api
 from ...core.model import UserPreference, User
 
+db_session = zeeguu.core.model.db.session
+
 
 @api.route("/user_preferences", methods=["GET"])
 @cross_domain
@@ -29,41 +31,41 @@ def save_user_preferences():
     audio_exercises_value = data.get(UserPreference.AUDIO_EXERCISES, None)
     if audio_exercises_value:
         pref = UserPreference.find_or_create(
-            zeeguu.core.model.db.session, user, UserPreference.AUDIO_EXERCISES
+            db_session, user, UserPreference.AUDIO_EXERCISES
         )
         pref.value = audio_exercises_value
-        zeeguu.core.model.db.session.add(pref)
+        db_session.add(pref)
 
     productive_exercises_value = data.get(UserPreference.PRODUCTIVE_EXERCISES, None)
     if productive_exercises_value:
         pref_productive = UserPreference.find_or_create(
-            zeeguu.core.model.db.session,
+            db_session,
             user,
             UserPreference.PRODUCTIVE_EXERCISES,
         )
         pref_productive.value = productive_exercises_value
-        zeeguu.core.model.db.session.add(pref_productive)
+        db_session.add(pref_productive)
 
     translate_reader_value = data.get(UserPreference.TRANSLATE_IN_READER, None)
     if translate_reader_value:
         translate_reader = UserPreference.find_or_create(
-            zeeguu.core.model.db.session,
+            db_session,
             user,
             UserPreference.TRANSLATE_IN_READER,
         )
         translate_reader.value = translate_reader_value
-        zeeguu.core.model.db.session.add(translate_reader)
-    
+        db_session.add(translate_reader)
+
     pronounce_reader_value = data.get(UserPreference.PRONOUNCE_IN_READER, None)
     if pronounce_reader_value:
         pronounce_reader = UserPreference.find_or_create(
-            zeeguu.core.model.db.session,
+            db_session,
             user,
             UserPreference.PRONOUNCE_IN_READER,
         )
         pronounce_reader.value = pronounce_reader_value
-        zeeguu.core.model.db.session.add(pronounce_reader)
+        db_session.add(pronounce_reader)
 
-    zeeguu.core.model.db.session.add(user)
-    zeeguu.core.model.db.session.commit()
+    db_session.add(user)
+    db_session.commit()
     return "OK"
