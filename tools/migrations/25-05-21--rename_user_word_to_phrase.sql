7a3adfe057c146204a505e69712e14a19239cf03
Mircea Filip Lungu <mircea.lungu@gmail.com>
2025-05-23 08:49:20 +0300

    completing the migration

--- tools/migrations/25-05-21--rename_user_word_to_phrase.sql
+++ tools/migrations/25-05-21--rename_user_word_to_phrase.sql
@@ -1,1 +1,10 @@
 alter table user_word RENAME TO phrase;
+
+ALTER TABLE phrase
+    DROP INDEX unique_word_language;
+
+
+ALTER TABLE phrase
+    CHANGE COLUMN word content VARCHAR(255)
+        CHARACTER SET utf8mb4
+            COLLATE utf8mb4_unicode_ci;
