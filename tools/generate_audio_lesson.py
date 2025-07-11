f6c228727265c6d283f2491f932bfcad3e4d13bc
Ada Lungu <ada.lungu@gmail.com>
2025-07-11 17:41:44 +0300

    refined promp, point to new prompt

--- tools/generate_audio_lesson.py
+++ tools/generate_audio_lesson.py
@@ -2,26 +2,30 @@ from zeeguu.api.app import create_app
 from zeeguu.core.audio_lessons.script_generator import generate_lesson_script
 from zeeguu.core.audio_lessons.voice_synthesizer import VoiceSynthesizer
 
-app = create_app()
-app.app_context().push()
+# app = create_app()
+# app.app_context().push()
 
+target_language = "es"
+cefr_level="A1"
 
 script = generate_lesson_script(
-    origin_word="overhovedet",
-    translation_word="before",
-    origin_language="da",
+    origin_word="mientras",
+    translation_word="while",
+    origin_language=target_language,
     translation_language="en",
-    cefr_level="A1",
-    generator_prompt_file="prompt_teacher_challenge_extra_outside_lesson.txt",
+    cefr_level=cefr_level,
+    generator_prompt_file="meaning_lesson--teacher_challenges_both_dialogue_and_beyond-v2.txt",
 )
+
+print(script)
 
 voice_synthesizer = VoiceSynthesizer()
 
 mp3_path = voice_synthesizer.generate_lesson_audio(
-    audio_lesson_meaning_id=44,
+    audio_lesson_meaning_id=51,
     script=script,
-    language_code="da",
-    cefr_level="A1",
+    language_code=target_language,
+    cefr_level=cefr_level,
 )
 
 print(mp3_path)
