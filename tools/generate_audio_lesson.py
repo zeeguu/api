b6afb152eea6949d2559120cb566090405baa06c
Ada Lungu <ada.lungu@gmail.com>
2025-07-15 17:09:08 +0300

    create scripts for all levels

--- tools/generate_audio_lesson.py
+++ tools/generate_audio_lesson.py
@@ -6,31 +6,60 @@ from zeeguu.core.audio_lessons.voice_synthesizer import VoiceSynthesizer
 # app.app_context().push()
 
 cefr_level_A1 = "A1"
+cefr_level_A2 = "A2"
 cefr_level_B1 = "B1"
+cefr_level_B2 = "B2"
+
 target_language = "es"
 
 script_A1 = generate_lesson_script(
-    origin_word="hasta",
-    translation_word="up to",
+    origin_word="sobre",
+    translation_word="especially",
     origin_language=target_language,
     translation_language="en",
     cefr_level=cefr_level_A1,
     generator_prompt_file="meaning_lesson--teacher_challenges_both_dialogue_and_beyond-v2.txt",
 )
 
+print("------------ A1----------")
 print(script_A1)
 
+script_A2 = generate_lesson_script(
+    origin_word="sobre",
+    translation_word="especially",
+    origin_language=target_language,
+    translation_language="en",
+    cefr_level=cefr_level_A2,
+    generator_prompt_file="meaning_lesson--teacher_challenges_both_dialogue_and_beyond-v2.txt",
+)
+
+print("------------ A2----------")
+print(script_A2)
+
+
 script_B1 = generate_lesson_script(
-    origin_word="hasta",
-    translation_word="up to",
+    origin_word="sobre",
+    translation_word="especially",
     origin_language=target_language,
     translation_language="en",
     cefr_level=cefr_level_B1,
     generator_prompt_file="meaning_lesson--teacher_challenges_both_dialogue_and_beyond-v2.txt",
 )
 
-print("------------ B1----------")
+print("------------ B1 ----------")
 print(script_B1)
+
+script_B2 = generate_lesson_script(
+    origin_word="sobre",
+    translation_word="especially",
+    origin_language=target_language,
+    translation_language="en",
+    cefr_level=cefr_level_B2,
+    generator_prompt_file="meaning_lesson--teacher_challenges_both_dialogue_and_beyond-v2.txt",
+)
+
+print("------------ B2----------")
+print(script_B2)
 
 voice_synthesizer = VoiceSynthesizer()
 
