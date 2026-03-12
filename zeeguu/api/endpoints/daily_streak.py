705f98349f246ee33fb6456434ed315fceb0efb2
kalnyzalan <kalny.zalan7@gmail.com>
2026-03-12 13:36:15 +0100

    Added endpoint for fetching daily streak information for all active languages

--- zeeguu/api/endpoints/daily_streak.py
+++ zeeguu/api/endpoints/daily_streak.py
@@ -19,3 +19,22 @@ def get_daily_streak():
         "max_streak": user_language.max_streak or 0,
         "max_streak_date": user_language.max_streak_date.strftime("%Y-%m-%d") if user_language.max_streak_date else None,
     })
+
+
+@api.route("/all_daily_streak", methods=["GET"])
+@cross_domain
+@requires_session
+def get_all_daily_streak():
+    user = User.find_by_id(flask.g.user_id)
+    user_languages = UserLanguage.all_user_languages_for_user(user)
+    result = []
+    for user_language in user_languages:
+        obj = {
+            "language": user_language.language.as_dictionary(),
+            "daily_streak": user_language.daily_streak or 0,
+            "max_streak": user_language.max_streak or 0,
+            "max_streak_date": user_language.max_streak_date.strftime(
+                "%Y-%m-%d") if user_language.max_streak_date else None
+        }
+        result.append(obj)
+    return json_result(result)
