eb9844d771959c007c3f7ff264f7cee5a5dcbd39
xXPinkmagicXx <nickras2001@gmail.com>
2026-04-12 09:27:06 +0200

    Working on the naming for the badge (levels)

--- tools/migrations/26-02-28--insert_default_badges.sql
+++ tools/migrations/26-02-28--insert_default_badges.sql
@@ -13,25 +13,26 @@ VALUES
 INSERT INTO badge (id, activity_type_id, level, threshold, name, icon_name)
 VALUES
         -- Translated Words
-        (1,  1, 1, 10,    NULL, 'translated-words-3.svg'),
-        (2,  1, 2, 100,   NULL, 'translated-words-3.svg'),
-        (3,  1, 3, 500,   NULL, 'translated-words-3.svg'),
-        (4,  1, 4, 1000,  NULL, 'translated-words-3.svg'),
-        (5,  1, 5, 2500,  NULL, 'translated-words-3.svg'),
+        (1,  1, 1, 10,    'Word Explorer', 'translated-words-3.svg'),
+        (2,  1, 2, 100,   'Vocabulary Builder', 'translated-words-3.svg'),
+        (3,  1, 3, 500,   'Meaning Mapper', 'translated-words-3.svg'),
+        (4,  1, 4, 1000,  'Lexicon Navigator', 'translated-words-3.svg'),
+        (5,  1, 5, 2500,  'Translation Master', 'translated-words-3.svg'),
+
 
         -- Correct Exercises
-        (6,  2, 1, 10,    NULL, 'correct-exercises-5.svg'),
-        (7,  2, 2, 250,   NULL, 'correct-exercises-5.svg'),
-        (8,  2, 3, 1000,  NULL, 'correct-exercises-5.svg'),
-        (9,  2, 4, 5000,  NULL, 'correct-exercises-5.svg'),
-        (10, 2, 5, 20000, NULL, 'correct-exercises-5.svg'),
+        (6,  2, 1, 10,    'Novice', 'correct-exercises-5.svg'),
+        (7,  2, 2, 250,   'Practitioner', 'correct-exercises-5.svg'),
+        (8,  2, 3, 1000,  'Proficient', 'correct-exercises-5.svg'),
+        (9,  2, 4, 5000,  'Expert', 'correct-exercises-5.svg'),
+        (10, 2, 5, 20000, 'Grandmaster', 'correct-exercises-5.svg'),
 
         -- Completed Audio Lessons
-        (11, 3, 1, 1,   NULL, 'completed-audio-lessons-4.svg'),
-        (12, 3, 2, 25,  NULL, 'completed-audio-lessons-4.svg'),
-        (13, 3, 3, 50,  NULL, 'completed-audio-lessons-4.svg'),
-        (14, 3, 4, 150, NULL, 'completed-audio-lessons-4.svg'),
-        (15, 3, 5, 300, NULL, 'completed-audio-lessons-4.svg'),
+        (11, 3, 1, 1,   'Audio Novice', 'completed-audio-lessons-4.svg'),
+        (12, 3, 2, 25,  'Sound Seeker', 'completed-audio-lessons-4.svg'),
+        (13, 3, 3, 50,  'Accent Explorer', 'completed-audio-lessons-4.svg'),
+        (14, 3, 4, 150, 'Fluent Listener', 'completed-audio-lessons-4.svg'),
+        (15, 3, 5, 300, 'Audio Virtuoso', 'completed-audio-lessons-4.svg'),
 
         -- Streak Count
         (16, 4, 1, 7,   NULL, 'streak-count-1.svg'),
@@ -41,22 +42,22 @@ VALUES
         (20, 4, 5, 365, NULL, 'streak-count-1.svg'),
 
         -- Learned Words
-        (21, 5, 1, 1,   NULL, 'learned-words-5.svg'),
-        (22, 5, 2, 10,  NULL, 'learned-words-5.svg'),
-        (23, 5, 3, 50,  NULL, 'learned-words-5.svg'),
-        (24, 5, 4, 250, NULL, 'learned-words-5.svg'),
-        (25, 5, 5, 750, NULL, 'learned-words-5.svg'),
+        (21, 5, 1, 1,   'Word Curious', 'learned-words-5.svg'),
+        (22, 5, 2, 10,  'Translation Enthusiast', 'learned-words-5.svg'),
+        (23, 5, 3, 50,  'Lexical Leader', 'learned-words-5.svg'),
+        (24, 5, 4, 250, 'Word Wizard', 'learned-words-5.svg'),
+        (25, 5, 5, 750, 'Polyglot Machine', 'learned-words-5.svg'),
 
         -- Read Articles
-        (26, 6, 1, 5,    NULL, 'read-articles-3.svg'),
-        (27, 6, 2, 25,   NULL, 'read-articles-3.svg'),
-        (28, 6, 3, 100,  NULL, 'read-articles-3.svg'),
-        (29, 6, 4, 500,  NULL, 'read-articles-3.svg'),
-        (30, 6, 5, 1000, NULL, 'read-articles-3.svg'),
+        (26, 6, 1, 5,    'Rookie Reader', 'read-articles-3.svg'),
+        (27, 6, 2, 25,   'Page Turner', 'read-articles-3.svg'),
+        (28, 6, 3, 100,  'Story Seeker', 'read-articles-3.svg'),
+        (29, 6, 4, 500,  'Reading Enthusiast', 'read-articles-3.svg'),
+        (30, 6, 5, 1000, 'Bookworm', 'read-articles-3.svg'),
 
         -- Number of Friends
-        (31, 7, 1, 1,  NULL, 'number-of-friends-1.svg'),
-        (32, 7, 2, 3,  NULL, 'number-of-friends-1.svg'),
-        (33, 7, 3, 5,  NULL, 'number-of-friends-1.svg'),
-        (34, 7, 4, 7,  NULL, 'number-of-friends-1.svg'),
-        (35, 7, 5, 10, NULL, 'number-of-friends-1.svg');
+        (31, 7, 1, 1,  'Newcomer', 'number-of-friends-1.svg'),
+        (32, 7, 2, 3,  'Connector', 'number-of-friends-1.svg'),
+        (33, 7, 3, 5,  'Socializer', 'number-of-friends-1.svg'),
+        (34, 7, 4, 7,  'Community Builder', 'number-of-friends-1.svg'),
+        (35, 7, 5, 10, 'Leader', 'number-of-friends-1.svg');
