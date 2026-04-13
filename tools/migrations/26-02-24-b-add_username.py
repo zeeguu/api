0d671e722504c8b54b5814bb4c819d8187dacb8c
xXPinkmagicXx <nickras2001@gmail.com>
2026-04-13 19:32:10 +0200

    more comments and better printing

--- tools/migrations/26-02-24-b-add_username.py
+++ tools/migrations/26-02-24-b-add_username.py
@@ -10,6 +10,12 @@ This script should be run after the database schema has been updated to include 
 26-02-24-a-add_username.sql should have been run first to add the 'username' column to the 'user' table.
 
 Run with: pip install -e . && python tools/migrations/26-02-24-b-add_username.py
+
+You might have to run this before: 
+set -a 
+source .env
+set +a
+to set the environment variables before running the script.
 """
 from zeeguu.core.model.user_avatar import UserAvatar
 from zeeguu.core.model.user import User
@@ -18,6 +24,7 @@ from zeeguu.core.model.user import User
 def populate_usernames():
     # Only query users who don't have a username set (i.e., those with username == None)
     users: list[User] = User.query.filter(User.username == None).all()
+    print(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')[:-3]} Found {len(users)} users without usernames. Populating now...")
     for user in users:
         user.username = User.generate_unique_username()
         user_avatar = UserAvatar.create_default_avatar_for_user(user)
@@ -28,10 +35,13 @@ def populate_usernames():
 if __name__ == "__main__":
     from zeeguu.api.app import create_app
     from zeeguu.core.model import db
-
+    from datetime import datetime
     app = create_app()
     app.app_context().push()
-
-    print("Starting random username population...")
+    start_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')[:-3]
+    print(f"{start_time} {'='*60}")
+    print(f"{start_time} STARTING - username population...")
     populate_usernames()
-    print("Username population completed.")
+    end_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')[:-3]
+    print(f"{end_time} COMPLETED - username population.")
+    print(f"{end_time} {'='*60}")
